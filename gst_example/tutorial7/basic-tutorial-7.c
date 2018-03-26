b1ac04f5b44c7f243290ad1a85efb312bb7fcac0
MyungJoo Ham <myungjoo.ham@samsung.com>
2018-03-26 20:43:48 +0900

    [Gst Example] tutorial-8
    GST Official Tutorial 8
    
    Signed-off-by: MyungJoo Ham <myungjoo.ham@samsung.com>

--- gst_example/tutorial7/basic-tutorial-7.c
+++ gst_example/tutorial7/basic-tutorial-7.c
@@ -65,10 +65,29 @@ int main(int argc, char *argv[]) {
 
   /* Start playing the pipeline */
   gst_element_set_state (pipeline, GST_STATE_PLAYING);
+  g_print ("Starting Playing\n");
 
   /* Wait until error or EOS */
   bus = gst_element_get_bus (pipeline);
   msg = gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE, GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+
+  if (msg != NULL) {
+    GError *err;
+    gchar *debug_info;
+
+    switch (GST_MESSAGE_TYPE (msg)) {
+      case GST_MESSAGE_ERROR:
+        gst_message_parse_error (msg, &err, &debug_info);
+        g_printerr ("Error received from element %s: %s\n", GST_OBJECT_NAME (msg->src), err->message);
+        g_printerr ("Debugging information: %s\n", debug_info ? debug_info : "none");
+        g_clear_error (&err);
+        g_free (debug_info);
+        break;
+      case GST_MESSAGE_EOS:
+        g_print ("End-Of-Stream reached.\n");
+        break;
+    }
+  }
 
   /* Release the request pads from the Tee, and unref them */
   gst_element_release_request_pad (tee, tee_audio_pad);
