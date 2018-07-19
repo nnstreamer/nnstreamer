3f06f724fac82690327a8c4c32b62ede399c8dd1
jy1210.jung <jy1210.jung@samsung.com>
2018-07-19 18:47:33 +0900

    [Test] move methods to generate test img
    to remove duplicated methods (for generating bmp) in testcase, move all
    generating functions to gen24bBMP.py.
    
    **Self evaluation:**
    1. Build test: [*]Passed [ ]Failed [ ]Skipped
    2. Run test: [ ]Passed [ ]Failed [* ]Skipped
    
    Signed-off-by: Jaeyun Jung <jy1210.jung@samsung.com>

--- tests/gen24bBMP.py
+++ tests/gen24bBMP.py
@@ -11,19 +11,20 @@
 from __future__ import print_function
 
 from struct import pack
+import random
 import sys
 
 
 ##
 # @brief Save bitmap "data" to "filename"
-# @param[in] filename The filename to be saves as a .png file.
+# @param[in] filename The filename to be saves as a .bmp file.
 # @param[in] data string of RGB (packed in 'BBB') or BGRx (packed in 'BBBB')
 # @param[in] colorspace "RGB" or "BGRx"
 # @param[in] width Width of the picture
 # @param[in] height Height of the picture
 def saveBMP(filename, data, colorspace, width, height):
     size = len(data)
-    graphics = ""
+    graphics = b''
 
     if colorspace == 'RGB':
         assert(size == (width * height * 3))
@@ -48,15 +49,295 @@ def saveBMP(filename, data, colorspace, width, height):
             for x in range(0, (width * 3) % 4):
                 graphics += pack('<B', 0)
     else:
-        print("Unrecognized colorspace %", colorspace)
+        print('Unrecognized colorspace %', colorspace)
         sys.exit(1)
 
     # BMP file header
-    header = ""
-    header += pack('<HLHHL', 19778, (26 + width * height * 3), 0, 0, 26)
+    header = pack('<HLHHL', 19778, (26 + width * height * 3), 0, 0, 26)
     header += pack('<LHHHH', 12, width, height, 1, 24)
 
-    file = open(filename, 'wb')
-    file.write(header)
-    file.write(graphics)
-    file.close()
+    with open(filename, 'wb') as file:
+        file.write(header)
+        file.write(graphics)
+
+
+##
+# @brief Write the generated data
+#
+def write(filename, data):
+    with open(filename, 'wb') as file:
+        file.write(data)
+
+
+##
+# @brief Generate Golden Test Case, a single videosrctest frame of 280x40xRGB
+# @return (string, string_size, expected_size)
+#
+# If string_size < expected_size, you do not need to check the results offset >= string_size.
+# string: binary string (b'\xff\x00....')
+def gen_RGB():
+    string = b''
+    string_size = 0
+    expected_size = 280 * 40 * 3
+    for i in range(0, 26):
+        # White
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBB', 255, 255, 255)
+        # Yellow
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBB', 255, 255, 0)
+        # Light Blue
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBB', 0, 255, 255)
+        # Green
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBB', 0, 255, 0)
+        # Purple
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBB', 255, 0, 255)
+        # Red
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBB', 255, 0, 0)
+        # Blue
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBB', 0, 0, 255)
+    for i in range(26, 30):
+        # Blue
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBB', 0, 0, 255)
+        # Black
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBB', 0, 0, 0)
+        # Purple
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBB', 255, 0, 255)
+        # Black
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBB', 0, 0, 0)
+        # Light Blue
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBB', 0, 255, 255)
+        # Black
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBB', 0, 0, 0)
+        # White
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBB', 255, 255, 255)
+    for i in range(0, 46):
+        # Dark Blue
+        string_size = string_size + 46
+        string += pack('BBB', 0, 0, 128)
+    for i in range(46, 93):
+        # White
+        string_size = string_size + 47
+        string += pack('BBB', 255, 255, 255)
+    for i in range(93, 140):
+        # Gray Blue
+        string_size = string_size + 47
+        string += pack('BBB', 0, 128, 255)
+    for i in range(140, 186):
+        # Black
+        string_size = string_size + 46
+        string += pack('BBB', 0, 0, 0)
+    for i in range(186, 210):
+        # Dark Gray
+        string_size = string_size + 24
+        string += pack('BBB', 19, 19, 19)
+    # We do not check the reset pixels: they are randomly generated.
+    string_size = string_size * 3
+    return string, string_size, expected_size
+
+
+##
+# @brief Generate Golden Test Case, a single videosrctest frame of 280x40xBGRx
+# @return (string, string_size, expected_size)
+#
+def gen_BGRx():
+    string = b''
+    string_size = 0
+    expected_size = 280 * 40 * 4
+    for i in range(0, 26):
+        # White
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBBB', 255, 255, 255, 255)
+        # Yellow
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBBB', 0, 255, 255, 255)
+        # Light Blue
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBBB', 255, 255, 0, 255)
+        # Green
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBBB', 0, 255, 0, 255)
+        # Purple
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBBB', 255, 0, 255, 255)
+        # Red
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBBB', 0, 0, 255, 255)
+        # Blue
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBBB', 255, 0, 0, 255)
+    for i in range(26, 30):
+        # Blue
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBBB', 255, 0, 0, 255)
+        # Black
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBBB', 0, 0, 0, 255)
+        # Purple
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBBB', 255, 0, 255, 255)
+        # Black
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBBB', 0, 0, 0, 255)
+        # Light Blue
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBBB', 255, 255, 0, 255)
+        # Black
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBBB', 0, 0, 0, 255)
+        # White
+        string_size = string_size + 40
+        for j in range(0, 40):
+            string += pack('BBBB', 255, 255, 255, 255)
+    for i in range(0, 46):
+        # Dark Blue
+        string_size = string_size + 46
+        string += pack('BBBB', 128, 0, 0, 255)
+    for i in range(46, 93):
+        # White
+        string_size = string_size + 47
+        string += pack('BBBB', 255, 255, 255, 255)
+    for i in range(93, 140):
+        # Gray Blue
+        string_size = string_size + 47
+        string += pack('BBBB', 255, 128, 0, 255)
+    for i in range(140, 186):
+        # Black
+        string_size = string_size + 46
+        string += pack('BBBB', 0, 0, 0, 255)
+    for i in range(186, 210):
+        # Dark Gray
+        string_size = string_size + 24
+        string += pack('BBBB', 19, 19, 19, 255)
+    # We do not check the reset pixels: they are randomly generated.
+    string_size = string_size * 4
+    return string, string_size, expected_size
+
+
+##
+# @brief Generate Golden Test Case, a randomly generated BMP image
+# @return (string, string_size, expected_size)
+#
+def gen_BMP_random(color_type, width, height, filename_prefix):
+    string = b''
+    string_size = 0
+    size_per_pixel = 3
+    if color_type == 'BGRx':
+        size_per_pixel = 4
+    expected_size = width * height * size_per_pixel
+    # The result has no stride for other/tensor types.
+
+    if color_type == 'BGRx':
+        for y in range(0, height):
+            for x in range(0, width):
+                pval = (random.randrange(256), random.randrange(256), random.randrange(256))
+                pixel = pack('BBBB', pval[2], pval[1], pval[0], 255)
+                string += pixel
+                string_size += 4
+    else:
+        # Assume RGB
+        for y in range(0, height):
+            for x in range(0, width):
+                pval = (random.randrange(256), random.randrange(256), random.randrange(256))
+                pixel = pack('BBB', pval[0], pval[1], pval[2])
+                string += pixel
+                string_size += 3
+
+    saveBMP(filename_prefix + '_' + color_type + '_' + str(width) + 'x' + str(height) + '.bmp',
+            string, color_type, width, height)
+    return string, string_size, expected_size
+
+
+##
+# @brief Generate a fixed BMP sequence for stream test
+# @return 0 if success. non-zero if failed.
+#
+# This gives "16x16", black, white, green, red, blue, wb-checker, rb-checker, gr-checker,
+# red-cross-on-white, blue-cross-on-black (4x4 x 16, left-top/right-bottom white/red/green).
+# "10 files" with 0 ~ 9 postfix in the filename
+def gen_BMP_stream(filename_prefix, golden_filename):
+    string = [b'' for _ in range(10)]
+    size_x = 16
+    size_y = 16
+
+    for y in range(0, size_y):
+        for x in range(0, size_x):
+            # black. Frame 0
+            string[0] += pack('BBB', 0, 0, 0)
+            # white. Frame 1
+            string[1] += pack('BBB', 255, 255, 255)
+            # green, Frame 2
+            string[2] += pack('BBB', 0, 255, 0)
+            # red, Frame 3
+            string[3] += pack('BBB', 255, 0, 0)
+            # blue, Frame 4
+            string[4] += pack('BBB', 0, 0, 255)
+            # white-black checker, Frame 5
+            if (((x / 4) % 2) + ((y / 4) % 2)) == 1:
+                string[5] += pack('BBB', 0, 0, 0)
+            else:
+                string[5] += pack('BBB', 255, 255, 255)
+            # red-blue checker, Frame 6
+            if (((x / 4) % 2) + ((y / 4) % 2)) == 1:
+                string[6] += pack('BBB', 0, 0, 255)
+            else:
+                string[6] += pack('BBB', 255, 0, 0)
+            # green-red checker, Frame 7
+            if (((x / 4) % 2) + ((y / 4) % 2)) == 1:
+                string[7] += pack('BBB', 255, 0, 0)
+            else:
+                string[7] += pack('BBB', 0, 255, 0)
+            # red-cross-on-white, Frame 8
+            if x == y:
+                string[8] += pack('BBB', 255, 0, 0)
+            else:
+                string[8] += pack('BBB', 255, 255, 255)
+            # blue-cross-on-black, Frame 9
+            if x == y:
+                string[9] += pack('BBB', 0, 0, 255)
+            else:
+                string[9] += pack('BBB', 0, 0, 0)
+
+    with open(golden_filename, 'wb') as file:
+        for i in range(0, 10):
+            file.write(string[i])
+            saveBMP(filename_prefix + '_' + str(i) + '.bmp', string[i], 'RGB', 16, 16)
--- tests/nnstreamer_tensors/generateGoldenTestResult.py
+++ tests/nnstreamer_tensors/generateGoldenTestResult.py
@@ -10,48 +10,10 @@
 
 from __future__ import print_function
 
-from struct import *
-import random
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 import gen24bBMP as bmp
 
-##
-# @brief Generate Golden Test Case 02, a randomly generated BMP image
-# @return (string, string_size, expected_size)
-#
-def genCase01_BMP_random(colorType, width, height):
-    string = ""
-    string_size = 0
-    sizePerPixel = 3
-    if (colorType == 'BGRx'):
-        sizePerPixel = 4
-    expected_size = width * height * sizePerPixel
 
-    # The result has no stride for other/tensor types.
-
-    if (colorType == 'BGRx'):
-        for y in range(0, height):
-            for x in range(0, width):
-                pval = (random.randrange(256), random.randrange(256), random.randrange(256))
-                pixel = pack('BBBB', pval[2], pval[1], pval[0], 255)
-                string += pixel
-                string_size += 4
-    else:
-        # Assume RGB
-        for y in range(0, height):
-            for x in range(0, width):
-                pval = (random.randrange(256), random.randrange(256), random.randrange(256))
-                pixel = pack('BBB', pval[0], pval[1], pval[2])
-                string += pixel
-                string_size += 3
-
-    bmp.saveBMP('testcase01_'+colorType+'_'+str(width)+'x'+str(height)+'.bmp', string, colorType, width, height)
-    return (string, string_size, expected_size)
-
-def write(filename, string):
-    newfile = open(filename, 'wb')
-    newfile.write(string)
-
-write('testcase01_RGB_640x480.golden', genCase01_BMP_random('RGB', 640, 480)[0])
+bmp.write('testcase01_RGB_640x480.golden', bmp.gen_BMP_random('RGB', 640, 480, 'testcase01')[0])
