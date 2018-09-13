8adb3ce972f42f297c392d67520f64561b89b18f
Geunsik Lim <geunsik.lim@samsung.com>
2018-09-13 18:41:24 +0900

    CI: Updated config files based on latest TAOS-CI version
    This commit is to update the existing configuration files
    based on the latest version of TAOS-CI.
    
    **Changes proposed in this PR:**
    1. Added config file for server administrator
    2. Updated the existing config files TAOS-CI version 1.0.20180913.
    
    Signed-off-by: Geunsik Lim <geunsik.lim@samsung.com>

---
+++ Documentation/ci-config/config-server-administrator.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+
+##
+# @file config-server-administrator.sh
+# @brief configuration file to declare contents that a server administrator installed.
+# @see      https://github.com/nnsuite/TAOS-CI
+# @author   Geunsik Lim <geunsik.lim@samsung.com>
+#
+
+########### Caution: If you are not server administrator, do not modify this file #################
+
+# Note that administrator of a server has to specify the location of eSDK at first.
+# In order to know how to install eSDK, please read plugins-base/pr-audit-build-yocto.sh file.
+# It is environment variables that are imported from eSDK to use devtool command.
+# - YOCTO_ESDK_DIR="/var/www"
+# - YOCTO_ESDK_NAME="kairos_sdk" or YOCTO_ESDK_NAME="poky_sdk"
+# In general, root path of Yocto eSDK is declated in $YOCTO_ESDK_DIR/$YOCTO_ESDK_NAME/ folder.
+
+YOCTO_ESDK_DIR="/var/www/"
+YOCTO_ESDK_NAME=""
+
