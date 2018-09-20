8e5f10ef7339ea0c06c4cb5f1b4dd4b56cce9cc7
Sangjung woo <sangjung.woo@samsung.com>
2018-09-20 16:36:33 +0900

    [TensorDecoder] Update the plugin information and its testcase
    This patch updates the plugin information such as Name, Description when
    getting by gst-inspect tool. Moreover, this updates its test case since
    the name is changed.
    
    Signed-off-by: Sangjung woo <sangjung.woo@samsung.com>

--- tests/nnstreamer_decoder/runTest.sh
+++ tests/nnstreamer_decoder/runTest.sh
@@ -17,7 +17,7 @@ function do_test {
     param="${4}_gen_golden_data"
     gstTest "--gst-plugin-path=${PATH_TO_PLUGIN} filesrc location=testcase01_${1}_${2}x${3}.png ! pngdec ! videoscale ! imagefreeze ! videoconvert ! video/x-raw,format=${1},width=${2},height=${3},framerate=0/1 ! filesink location=\"testcase01_${1}_${2}x${3}.golden.raw\" sync=true" $param
 
-    gstTest "--gst-plugin-path=${PATH_TO_PLUGIN} filesrc location=testcase01_${1}_${2}x${3}.png ! pngdec ! videoscale ! imagefreeze ! videoconvert ! video/x-raw,format=${1},width=${2},height=${3},framerate=0/1 ! tensor_converter ! tensordec silent=false ! filesink location=\"testcase01_${1}_${2}x${3}.log\" sync=true" ${4}
+    gstTest "--gst-plugin-path=${PATH_TO_PLUGIN} filesrc location=testcase01_${1}_${2}x${3}.png ! pngdec ! videoscale ! imagefreeze ! videoconvert ! video/x-raw,format=${1},width=${2},height=${3},framerate=0/1 ! tensor_converter ! tensor_decoder silent=false ! filesink location=\"testcase01_${1}_${2}x${3}.log\" sync=true" ${4}
 
     compareAllSizeLimit testcase01_${1}_${2}x${3}.log testcase01_${1}_${2}x${3}.golden.raw ${4}
 }
@@ -28,7 +28,7 @@ do_test BGRx 640 480 1-2
 do_test BGRx 642 480 1-3
 
 # Test with a stream of 10 small PNG frames
-gstTest "--gst-plugin-path=${PATH_TO_PLUGIN} multifilesrc location=\"testsequence_%1d.png\" index=0 caps=\"image/png,framerate=\(fraction\)30/1\" ! pngdec ! videoconvert ! video/x-raw, format=RGB ! tensor_converter ! tee name=t ! queue ! tensordec ! filesink location=\"testcase2.dec.log\" sync=true t. ! queue ! filesink location=\"testcase2.con.log\" sync=true" 2
+gstTest "--gst-plugin-path=${PATH_TO_PLUGIN} multifilesrc location=\"testsequence_%1d.png\" index=0 caps=\"image/png,framerate=\(fraction\)30/1\" ! pngdec ! videoconvert ! video/x-raw, format=RGB ! tensor_converter ! tee name=t ! queue ! tensor_decoder ! filesink location=\"testcase2.dec.log\" sync=true t. ! queue ! filesink location=\"testcase2.con.log\" sync=true" 2
 compareAll testcase2.con.log testcase2.dec.log 2
 
 report
