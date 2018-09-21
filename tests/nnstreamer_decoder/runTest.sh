1698cccc6908e63c71fc05dbf381c88dca86e16e
Jaeyun <jy1210.jung@samsung.com>
2018-09-21 17:26:14 +0900

    [Test/Decode] remove debug message
    remove the property silent in tensor decoder testcase.
    
    Signed-off-by: Jaeyun Jung <jy1210.jung@samsung.com>

--- tests/nnstreamer_decoder/runTest.sh
+++ tests/nnstreamer_decoder/runTest.sh
@@ -17,7 +17,7 @@ function do_test {
     param="${4}_gen_golden_data"
     gstTest "--gst-plugin-path=${PATH_TO_PLUGIN} filesrc location=testcase01_${1}_${2}x${3}.png ! pngdec ! videoscale ! imagefreeze ! videoconvert ! video/x-raw,format=${1},width=${2},height=${3},framerate=0/1 ! filesink location=\"testcase01_${1}_${2}x${3}.golden.raw\" sync=true" $param
 
-    gstTest "--gst-plugin-path=${PATH_TO_PLUGIN} filesrc location=testcase01_${1}_${2}x${3}.png ! pngdec ! videoscale ! imagefreeze ! videoconvert ! video/x-raw,format=${1},width=${2},height=${3},framerate=0/1 ! tensor_converter ! tensor_decoder silent=false ! filesink location=\"testcase01_${1}_${2}x${3}.log\" sync=true" ${4}
+    gstTest "--gst-plugin-path=${PATH_TO_PLUGIN} filesrc location=testcase01_${1}_${2}x${3}.png ! pngdec ! videoscale ! imagefreeze ! videoconvert ! video/x-raw,format=${1},width=${2},height=${3},framerate=0/1 ! tensor_converter ! tensor_decoder ! filesink location=\"testcase01_${1}_${2}x${3}.log\" sync=true" ${4}
 
     compareAllSizeLimit testcase01_${1}_${2}x${3}.log testcase01_${1}_${2}x${3}.golden.raw ${4}
 }
