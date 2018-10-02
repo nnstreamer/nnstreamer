cb5bf8f311512f30c527555b74ce512639a9317f
jijoong.moon <jijoong.moon@samsung.com>
2018-10-02 15:22:47 +0900

    [TensorMerge] Add  draft tensor_merge plugin
    we need tensor_merge to merge tensor. It make multiple tensor into
    single tensor. After this PR, C:W:H:B ([B][H][W][C]) +
    C:W:H:B ([B][H][W][C]) --> W:H:2xC:B ([B][2xC][H][W]).
    Therefore, if you want to use this as input for tensorflow lite, you
    may need transform element,
    with transpose mode. ( mode=transpose, option=2:0:1:3. It produce
    2xC:W:H:B ([B][H][W][2xC]).
    
    **Changes proposed in this PR:**
    - Added Tensor merge plugin
    
    **TODO**
    - Add Multiple direction merge : Width, Height or mixed.
    - Better handling to reduce memcopy.
    
    **Self evaluation:**
    1. Build test:	 [X]Passed [ ]Failed [ ]Skipped
    2. Run test:	  [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: jijoong.moon <jijoong.moon@samsung.com>

---
+++ gst/tensor_merge/gsttensormerge.h
@@ -0,0 +1,119 @@
+/**
+ * GStreamer
+ * Copyright (C) 2005 Thomas Vander Stichele <thomas@apestaart.org>
+ * Copyright (C) 2005 Ronald S. Bultje <rbultje@ronald.bitfreak.net>
+ * Copyright (C) 2018 Jijoong Moon <jijoong.moon@samsung.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ */
+/**
+ * @file	gsttensormerge.h
+ * @date	03 July 2018
+ * @brief	GStreamer plugin to merge tensors (as a filter for other general neural network filters)
+ * @see		https://github.com/nnsuite/nnstreamer
+ * @author	Jijoong Moon <jijoong.moon@samsung.com>
+ * @bug		No known bugs except for NYI items
+ *
+ */
+
+#ifndef __GST_TENSOR_MERGE_H__
+#define __GST_TENSOR_MERGE_H__
+
+#include <gst/gst.h>
+#include <gst/base/gstcollectpads.h>
+#include <tensor_common.h>
+
+G_BEGIN_DECLS
+#define GST_TYPE_TENSOR_MERGE (gst_tensor_merge_get_type ())
+#define GST_TENSOR_MERGE(obj) (G_TYPE_CHECK_INSTANCE_CAST ((obj), GST_TYPE_TENSOR_MERGE, GstTensorMerge))
+#define GST_TENSOR_MERGE_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST ((klass), GST_TYPE_TENSOR_MERGE, GstTensorMergeClass))
+#define GST_TENSOR_MERGE_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS ((obj), GST_TYPE_TENSOR_MERGE, GstTensorMergeClass))
+#define GST_IS_TENSOR_MERGE(obj) (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_TENSOR_MERGE))
+#define GST_IS_TENSOR_MERGE_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_TENSOR_MERGE))
+#define GST_TENSOR_MERGE_CAST(obj)((GstTensorMerge*)(obj))
+typedef struct _GstTensorMerge GstTensorMerge;
+typedef struct _GstTensorMergeClass GstTensorMergeClass;
+
+typedef enum
+{
+  GTT_LINEAR = 0,               /* Dimension Change. "dimchg" */
+  GTT_END,
+} tensor_merge_mode;
+
+typedef enum
+{
+  LINEAR_WIDTH = 0,
+  LINEAR_HEIGHT = 1,
+  LINEAR_CHANNEL = 2,
+  LINEAR_END,
+} tensor_merge_linear_mode;
+
+
+/**
+ * @brief Internal data structure for linear mode.
+ */
+typedef struct _tensor_merge_linear {
+  tensor_merge_linear_mode direction;
+} tensor_merge_linear;
+
+
+typedef struct
+{
+  GstCollectData collect;
+  GstBuffer *buffer;
+  GstClockTime pts_timestamp;
+  GstClockTime dts_timestamp;
+  GstPad *pad;
+
+  gboolean have_timestamp_offset;
+} GstTensorMergePadData;
+
+/**
+ * @brief Tensor Merge data structure
+ */
+struct _GstTensorMerge
+{
+  GstElement element;
+
+  gboolean silent;
+  GstPad *srcpad;
+  gchar *option;
+  tensor_merge_mode mode;
+  union{
+    tensor_merge_linear data_linear;
+  };
+
+  gboolean loaded;
+  GstCollectPads *collect;
+  gboolean negotiated;
+  gboolean need_segment;
+  gboolean need_stream_start;
+  gboolean send_stream_start;
+
+  GstTensorsConfig tensors_config; /**< output tensors info */
+};
+
+/**
+ * @brief GstTensorMergeClass inherits GstElementClass
+ */
+struct _GstTensorMergeClass
+{
+  GstElementClass parent_class;
+};
+
+/**
+ * @brief Get Type function required for gst elements
+ */
+GType gst_tensor_merge_get_type (void);
+
+G_END_DECLS
+#endif  /** __GST_TENSOR_MERGE_H__ **/
