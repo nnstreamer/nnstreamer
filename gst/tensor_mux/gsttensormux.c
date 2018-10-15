3727411d043ce7101e1d2f63ce870aaf239aab97
Jaeyun <jy1210.jung@samsung.com>
2018-10-15 16:00:33 +0900

    [Mux/Merge] remove unnecessary value
    1. remove unnecessary value in pad-data structure
    2. add cond to check valid buffer
    3. add code to free option string
    
    Signed-off-by: Jaeyun Jung <jy1210.jung@samsung.com>

--- gst/tensor_mux/gsttensormux.c
+++ gst/tensor_mux/gsttensormux.c
@@ -87,8 +87,6 @@ static GstStaticPadTemplate sink_templ = GST_STATIC_PAD_TEMPLATE ("sink_%u",
     GST_STATIC_CAPS (GST_TENSOR_CAP_DEFAULT)
     );
 
-static void gst_tensor_mux_finalize (GObject * object);
-
 static gboolean gst_tensor_mux_handle_src_event (GstPad * pad,
     GstObject * parent, GstEvent * event);
 static GstPad *gst_tensor_mux_request_new_pad (GstElement * element,
@@ -104,6 +102,7 @@ static void gst_tensor_mux_set_property (GObject * object, guint prop_id,
     const GValue * value, GParamSpec * pspec);
 static void gst_tensor_mux_get_property (GObject * object, guint prop_id,
     GValue * value, GParamSpec * pspec);
+static void gst_tensor_mux_finalize (GObject * object);
 
 #define gst_tensor_mux_parent_class parent_class
 G_DEFINE_TYPE (GstTensorMux, gst_tensor_mux, GST_TYPE_ELEMENT);
@@ -188,8 +187,11 @@ gst_tensor_mux_finalize (GObject * object)
   GstTensorMux *tensor_mux;
   tensor_mux = GST_TENSOR_MUX (object);
 
-  if (tensor_mux->collect)
+  if (tensor_mux->collect) {
     gst_object_unref (tensor_mux->collect);
+    tensor_mux->collect = NULL;
+  }
+
   G_OBJECT_CLASS (parent_class)->finalize (object);
 }
 
@@ -273,26 +275,26 @@ gst_tensor_mux_sink_event (GstCollectPads * pads, GstCollectData * data,
 /**
  * @brief Compare dts & pts time and find earliest
  * @param tensor_mux tensor muxer
- * @param old previous mux pad data
- * @param new current mux pad data
+ * @param old_data previous mux pad data
+ * @param new_data current mux pad data
  * @return if > 0, new is earlier than old
  */
 static gint
 gst_tensor_mux_compare_pads (GstTensorMux * tensor_mux,
-    GstTensorMuxPadData * old, GstTensorMuxPadData * new)
+    GstTensorMuxPadData * old_data, GstTensorMuxPadData * new_data)
 {
   guint64 oldtime, newtime;
-  if (old == NULL || old->buffer == NULL)
+  if (old_data == NULL)
     return 1;
-  if (new == NULL || new->buffer == NULL)
+  if (new_data == NULL)
     return -1;
-  if (GST_CLOCK_TIME_IS_VALID (old->dts_timestamp) &&
-      GST_CLOCK_TIME_IS_VALID (new->dts_timestamp)) {
-    oldtime = old->dts_timestamp;
-    newtime = new->dts_timestamp;
+  if (GST_CLOCK_TIME_IS_VALID (old_data->dts_timestamp) &&
+      GST_CLOCK_TIME_IS_VALID (new_data->dts_timestamp)) {
+    oldtime = old_data->dts_timestamp;
+    newtime = new_data->dts_timestamp;
   } else {
-    oldtime = old->pts_timestamp;
-    newtime = new->pts_timestamp;
+    oldtime = old_data->pts_timestamp;
+    newtime = new_data->pts_timestamp;
   }
 
   if (!GST_CLOCK_TIME_IS_VALID (oldtime))
@@ -348,34 +350,33 @@ gst_tensor_mux_collect_buffer (GstTensorMux * tensor_mux,
     gst_caps_unref (caps);
 
     walk = g_slist_next (walk);
+
     GstBuffer *buf = NULL;
     buf = gst_collect_pads_pop (tensor_mux->collect, data);
-    if (buf && GST_BUFFER_PTS_IS_VALID (buf)) {
-      pad->pts_timestamp =
-          gst_segment_to_running_time (&data->segment, GST_FORMAT_TIME,
-          GST_BUFFER_PTS (buf));
-    } else {
-      pad->pts_timestamp = GST_CLOCK_TIME_NONE;
-    }
-    if (buf && GST_BUFFER_DTS_IS_VALID (buf)) {
-      pad->dts_timestamp =
-          gst_segment_to_running_time (&data->segment, GST_FORMAT_TIME,
-          GST_BUFFER_DTS (buf));
-    } else {
-      pad->dts_timestamp = GST_CLOCK_TIME_NONE;
-    }
 
-    pad->buffer = buf;
     if (GST_IS_BUFFER (buf)) {
+      if (GST_BUFFER_PTS_IS_VALID (buf)) {
+        pad->pts_timestamp =
+            gst_segment_to_running_time (&data->segment, GST_FORMAT_TIME,
+            GST_BUFFER_PTS (buf));
+      } else {
+        pad->pts_timestamp = GST_CLOCK_TIME_NONE;
+      }
+      if (GST_BUFFER_DTS_IS_VALID (buf)) {
+        pad->dts_timestamp =
+            gst_segment_to_running_time (&data->segment, GST_FORMAT_TIME,
+            GST_BUFFER_DTS (buf));
+      } else {
+        pad->dts_timestamp = GST_CLOCK_TIME_NONE;
+      }
+
       mem = gst_buffer_get_memory (buf, 0);
       gst_buffer_append_memory (tensors_buf, mem);
 
-      if (pad->buffer != NULL) {
-        if (gst_tensor_mux_compare_pads (tensor_mux, bestpad, pad) > 0) {
-          bestpad = pad;
-          *pts_time = bestpad->pts_timestamp;
-          *dts_time = bestpad->dts_timestamp;
-        }
+      if (gst_tensor_mux_compare_pads (tensor_mux, bestpad, pad) > 0) {
+        bestpad = pad;
+        *pts_time = bestpad->pts_timestamp;
+        *dts_time = bestpad->dts_timestamp;
       }
 
       gst_buffer_unref (buf);
@@ -426,6 +427,7 @@ gst_tensor_mux_collected (GstCollectPads * pads, GstTensorMux * tensor_mux)
 
   tensors_buf = gst_buffer_new ();
   g_assert (tensors_buf);
+
   isEOS =
       gst_tensor_mux_collect_buffer (tensor_mux, tensors_buf, &pts_time,
       &dts_time);
@@ -479,6 +481,7 @@ beach:
   return ret;
 nego_error:
   {
+    gst_buffer_unref (tensors_buf);
     GST_WARNING_OBJECT (tensor_mux, "failed to set caps");
     GST_ELEMENT_ERROR (tensor_mux, CORE, NEGOTIATION, (NULL), (NULL));
     return GST_FLOW_NOT_NEGOTIATED;
