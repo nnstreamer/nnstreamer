0d57f0989acd94d7378790ab2a9932182769bd88
jijoong.moon <jijoong.moon@samsung.com>
2018-11-05 10:24:27 +0900

    [Transform] Add testcases for transpose
    Add thress trasnpose test cases
    1. Tensor 100:50:3:1  --> Tensor 3:100:50:1
    2. Tensor 200:100:3:1  --> Tensor 3:200:100:1
    3. Tensor 200:100:3:1  --> Tensor 100:200:3:1
    
    **Self evaluation:**
    1. Build test:	 [X]Passed [ ]Failed [ ]Skipped
    2. Run test:	 [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: jijoong.moon <jijoong.moon@samsung.com>

---
+++ tests/transform_transpose/generateTest.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+##
+# Copyright (C) 2018 Samsung Electronics
+# License: LGPL-2.1
+#
+# @file generateTest.py
+# @brief Generate golden test results for test cases
+# @author Jijoong Moon <jijoong.moon@samsung.com>
+
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from struct import pack
+import random
+
+import numpy as np
+
+def saveTestData(filename, channel, height, width, batch, idx_i, idx_j, idx_k, idx_l):
+    string = b''
+    data = []
+
+    for b in range(0,batch):
+        for c in range(0,channel):
+            for h in range(0,height):
+                for w in range(0,width):
+                    n = random.uniform(0.0, 10.0)
+                    string += pack('f', n)
+                    data.append(n)
+
+    with open(filename,'wb') as file:
+        file.write(string)
+
+    file.close()
+
+    a=np.ndarray((batch, channel, height, width), np.float32);
+
+    for b in range(0,batch):
+        for c in range(0,channel):
+            for h in range(0,height):
+                for w in range(0,width):
+                    a[b][c][h][w] = data[b*channel*height*width+c*height*width+h*width+w]
+
+    a=np.transpose(a, (idx_i,idx_j,idx_k,idx_l))
+
+    a=a.copy(order='C')
+
+    with open(filename+".golden",'wb') as file1:
+        file1.write(a)
+    file1.close()
+
+    return a
+
+buf = saveTestData("test01_00.dat", 3, 50, 100, 1, 0, 2, 3, 1)
+
+buf = saveTestData("test02_00.dat", 3, 100, 200, 1, 0, 2, 3, 1)
+
+buf = saveTestData("test03_00.dat", 3, 100, 200, 1, 0, 1, 3, 2)
