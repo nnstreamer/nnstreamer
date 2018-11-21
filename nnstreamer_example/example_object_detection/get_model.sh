243a89e3276ff666fc0001af97439a61e1ae132a
MyungJoo Ham <myungjoo.ham@samsung.com>
2018-11-21 15:11:22 +0900

    [Example] ObjectDetection(SSD) with Gst-Launch
    With the new decoder/bounding-box, we can do
    object-detection + labeling in gst-launch with
    visualization.
    
    Signed-off-by: MyungJoo Ham <myungjoo.ham@samsung.com>

---
+++ nnstreamer_example/example_object_detection/get_model.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+wget https://github.com/nnsuite/testcases/raw/master/DeepLearningModels/tensorflow-lite/ssd_mobilenet_v2_coco/ssd_mobilenet_v2_coco.tflite
+wget https://github.com/nnsuite/testcases/raw/master/DeepLearningModels/tensorflow-lite/ssd_mobilenet_v2_coco/coco_labels_list.txt
+wget https://github.com/nnsuite/testcases/raw/master/DeepLearningModels/tensorflow-lite/ssd_mobilenet_v2_coco/box_priors.txt
