bba3cb00599064246d06a8a9d1824d25372ede0e
Jaeyun <jy1210.jung@samsung.com>
2018-11-22 14:11:27 +0900

    [Test] update git ignore
    add .xraw file and change temp file format in unit-test
    
    Signed-off-by: Jaeyun Jung <jy1210.jung@samsung.com>

--- tests/nnstreamer_decoder_image_labeling/runTest.sh
+++ tests/nnstreamer_decoder_image_labeling/runTest.sh
@@ -22,7 +22,7 @@ testInit $1
 PATH_TO_MODEL="../test_models/models/mobilenet_v1_1.0_224_quant.tflite"
 PATH_TO_LABEL="../test_models/labels/labels.txt"
 PATH_TO_IMAGE="img/orange.png"
-PATH_TO_FILE="tensordecoder.txt"
+PATH_TO_FILE="tensordecoder.log"
 
 gstTest "--gst-plugin-path=${PATH_TO_PLUGIN} filesrc location=\"${PATH_TO_IMAGE}\" ! pngdec ! videoscale ! imagefreeze ! videoconvert ! video/x-raw, format=RGB, framerate=0/1 ! tensor_converter ! tensor_filter framework=\"tensorflow-lite\" model=\"${PATH_TO_MODEL}\" ! tensor_decoder mode=image_labeling option1=\"${PATH_TO_LABEL}\" ! filesink location=\"${PATH_TO_FILE}\"" D1 0 0 $PERFORMANCE
 
