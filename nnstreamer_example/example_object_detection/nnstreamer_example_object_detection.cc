f6847475ba2f4970eb24d0d7e13bb7fd721b9e41
Jaeyun <jy1210.jung@samsung.com>
2018-11-23 13:54:22 +0900

    [Example] update arithmetic for normalization
    update arith mode in tensor-transform to normalize
    
    Signed-off-by: Jaeyun Jung <jy1210.jung@samsung.com>

--- nnstreamer_example/example_object_detection/nnstreamer_example_object_detection.cc
+++ nnstreamer_example/example_object_detection/nnstreamer_example_object_detection.cc
@@ -674,7 +674,7 @@ main (int argc, char ** argv)
       "video/x-raw,width=%d,height=%d,format=RGB ! tee name=t_raw "
       "t_raw. ! queue ! videoconvert ! cairooverlay name=tensor_res ! ximagesink name=img_tensor "
       "t_raw. ! queue leaky=2 max-size-buffers=2 ! videoscale ! video/x-raw,width=%d,height=%d ! tensor_converter ! "
-      "tensor_transform mode=arithmetic option=typecast:float32,add:-127,mul:0.007843 ! "
+      "tensor_transform mode=arithmetic option=typecast:float32,add:-127.5,div:127.5 ! "
       "tensor_filter framework=tensorflow-lite model=%s ! "
       "tensor_sink name=tensor_sink",
       VIDEO_WIDTH, VIDEO_HEIGHT, MODEL_WIDTH, MODEL_HEIGHT,
