11f2b01f1e404eb9e1e3e00b4ff7f8b3ce7e6211
Hyoung Joo Ahn <hello.ahnn@gmail.com>
2019-01-08 11:24:22 +0900

    [Example] Upload the tensorflow example
    this example detects objects with ssd model. it works similar with the tf-lite example
    
    Signed-off-by: Hyoung Joo Ahn <hello.ahnn@gmail.com>

---
+++ nnstreamer_example/example_object_detection_tensorflow/get_model.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+wget https://github.com/nnsuite/testcases/blob/master/DeepLearningModels/tensorflow/ssdlite_mobilenet_v2/ssdlite_mobilenet_v2.pb
+wget https://github.com/nnsuite/testcases/blob/master/DeepLearningModels/tensorflow/ssdlite_mobilenet_v2/coco_labels_list.txt
