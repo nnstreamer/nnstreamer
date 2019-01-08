0e56b86a2f041006742d32ab2d4909a65a7ff47b
Jaeyun <jy1210.jung@samsung.com>
2019-01-08 14:12:40 +0900

    [Build] change dir structure
    1. update meson/cmake to change directory structure (move common files and plugins to gst/nnstreamer)
    2. move nnstreamer meson-build to gst/nnstreamer
    3. set project args to build nnstreamer
    
    Signed-off-by: Jaeyun Jung <jy1210.jung@samsung.com>

--- tests/nnstreamer_plugins/unittest_plugins.cpp
+++ tests/nnstreamer_plugins/unittest_plugins.cpp
@@ -2096,7 +2096,7 @@ TEST (test_tensor_aggregator, aggregate_4)
 }
 
 #ifdef HAVE_ORC
-#include "../../gst/tensor_transform/transform-orc.h"
+#include "../../gst/nnstreamer/tensor_transform/transform-orc.h"
 
 /**
  * @brief Test for tensor_transform orc functions (add constant value)
