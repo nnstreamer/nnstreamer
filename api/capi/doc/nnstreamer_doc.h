23c0dff2cd455433fa4ee01201d11ff38e4cdede
Jaeyun <jy1210.jung@samsung.com>
2019-07-05 14:57:29 +0900

    [API] fix typo in doc header
    update doc-header file (fix typo and minor changes)
    
    Signed-off-by: Jaeyun Jung <jy1210.jung@samsung.com>

--- api/capi/doc/nnstreamer_doc.h
+++ api/capi/doc/nnstreamer_doc.h
@@ -27,11 +27,11 @@
  * @ingroup CAPI_ML_FRAMEWORK
  * @defgroup CAPI_ML_NNSTREAMER NNStreamer
  * @brief NNStreamer allows to construct and execute a GStreamer pipeline including neural networks.\n
- * NNStreamer is a set of GStreamer plugins that allow GStreamer developers to adopt neural network models easily and efficiently and neural network developers to manage stream pipelines and their filters easily and efficiently. \n
- * https://github.com/nnsuite/nnstreamer/ \n
+ * NNStreamer is a set of GStreamer plugins that allow GStreamer developers to adopt neural network models easily and efficiently and neural network developers to manage stream pipelines and their filters easily and efficiently.\n
+ * https://github.com/nnsuite/nnstreamer/\n
  * \n
  * Main objectives of NNStreamer include:\n
- * * Provide neural network framework connectivities (e.g., tensorflow, caffe) for GStreamer streams.\n
+ * * Provide neural network framework connectivity (e.g., tensorflow, caffe) for GStreamer streams.\n
  *     * **Efficient Streaming for AI Projects**: Apply efficient and flexible stream pipeline to neural networks.\n
  *     * **Intelligent Media Filters!**: Use a neural network model as a media filter / converter.\n
  *     * **Composite Models!**: Multiple neural network models in a single stream pipeline instance.\n
@@ -42,27 +42,29 @@
  * \n
  * There are following sub groups proposed:\n
  * * Pipeline: construct and control an NNStreamer pipeline.\n
+ * * Single: invoke a neural network model with a single instance of input data.\n
  *
  * @defgroup CAPI_ML_NNSTREAMER_PIPELINE_MODULE NNStreamer Pipeline
  * @ingroup  CAPI_ML_NNSTREAMER
  * @brief The NNStreamer function provides interfaces to create and execute stream pipelines with neural networks and sensors.
  * @section CAPI_ML_NNSTREAMER_PIPELINE_HEADER Required Header
- *   \#include <nnstreamer/nnstreamer.h> \n
+ *   \#include <nnstreamer/nnstreamer.h>\n
  *
  * @section CAPI_ML_NNSTREAMER_PIPELINE_OVERVIEW Overview
  * The NNStreamer function provides interfaces to create and execute stream pipelines with neural networks and sensors.
  *
  *  This function allows the following operations with NNStreamer:
- * - Create a stream pipeline with NNStreamer plugins, GStreamer plugins, and sensor/camera/mic inputs
- * - Interfaces to push data to the pipeline from the application
- * - Interfaces to pull data from the pipeline to the application
- * - Interfaces to start/stop/destroy the pipeline
+ * - Create a stream pipeline with NNStreamer plugins, GStreamer plugins, and sensor/camera/mic inputs.
+ * - Interfaces to push data to the pipeline from the application.
+ * - Interfaces to pull data from the pipeline to the application.
+ * - Interfaces to start/stop/destroy the pipeline.
  * - Interfaces to control switches and valves in the pipeline.
+ * - Utility functions to handle data from the pipeline.
  *
  *  Note that this function set is supposed to be thread-safe.
  *
  * @section CAPI_ML_NNSTREAMER_PIPELINE_FEATURE Related Features
- * This function is related with the following features :\n
+ * This function is related with the following features:\n
  *  - http://tizen.org/feature/nnstreamer.pipeline\n
  *
  * It is recommended to probe features in your application for reliability.\n
@@ -83,18 +85,18 @@
  * @ingroup  CAPI_ML_NNSTREAMER
  * @brief The NNStreamer Single API provides interfaces to invoke a neural network model with a single instance of input data.
  * @section CAPI_ML_NNSTREAMER_SINGLE_HEADER Required Header
- *   \#include <nnstreamer/nnstreamer-single.h> \n
+ *   \#include <nnstreamer/nnstreamer-single.h>\n
  *
  * @section CAPI_ML_NNSTREAMER_SINGLE_OVERVIEW Overview
  * The NNStreamer Single API provides interfaces to invoke a neural network model with a single instance of input data.
  * This function is a syntactic sugar of NNStreamer Pipeline API with simplified features; thus, users are supposed to use NNStreamer Pipeline API directly if they want more advanced features.
  * The user is expected to preprocess the input data for the given neural network model.
  *
- * This function allows the following operations with NNSTreamer:
- * - Open a machine learning model (ml_model) with various mechanisms.
- * - Close the model
+ * This function allows the following operations with NNStreamer:
+ * - Open a machine learning model with various mechanisms.
+ * - Close the model.
  * - Interfaces to enter a single instance of input data to the opened model.
- * - Utility functions to handle opened model.
+ * - Utility functions to get the information of opened model.
  *
  * Note that this function set is supposed to be thread-safe.
  *
@@ -102,7 +104,7 @@
  * This function is related with the following features:\n
  *  - http://tizen.org/feature/nnstreamer.single\n
  *
- * It is recommended to probe feaqtures in your applicatoin for reliability.\n
+ * It is recommended to probe features in your application for reliability.\n
  * You can check if a device supports the related features for this function by using
  * @ref CAPI_SYSTEM_SYSTEM_INFO_MODULE, thereby controlling the procedure of
  * your application.\n
