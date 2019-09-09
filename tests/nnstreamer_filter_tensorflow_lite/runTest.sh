fa29be92abca96a76d1173176c33365ed6ea3519
jijoong.moon <jijoong.moon@samsung.com>
2019-09-09 11:11:11 +0900

    [TEST] Fix nnapi test
    Fix nnapi test (#1647)
    
    **Self evaluation:**
    1. Build test:	 [X]Passed [ ]Failed [ ]Skipped
    2. Run test:	 [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: jijoong.moon <jijoong.moon@samsung.com>

--- tests/nnstreamer_filter_tensorflow_lite/runTest.sh
+++ tests/nnstreamer_filter_tensorflow_lite/runTest.sh
@@ -67,7 +67,6 @@ fi
 PATH_TO_MODEL="../test_models/models/mobilenet_v1_1.0_224_quant.tflite"
 PATH_TO_LABEL="../test_models/labels/labels.txt"
 PATH_TO_IMAGE="../test_models/data/orange.png"
-SYSTEM=`uname -m`
 
 gstTest "--gst-plugin-path=${PATH_TO_PLUGIN} filesrc location=${PATH_TO_IMAGE} ! pngdec ! videoscale ! imagefreeze ! videoconvert ! video/x-raw,format=RGB,framerate=0/1 ! tensor_converter ! tensor_filter framework=tensorflow-lite model=${PATH_TO_MODEL} ! filesink location=tensorfilter.out.log" 1 0 0 $PERFORMANCE
 python checkLabel.py tensorfilter.out.log ${PATH_TO_LABEL} orange
@@ -80,28 +79,21 @@ gstTest "--gst-plugin-path=${PATH_TO_PLUGIN} filesrc location=${PATH_TO_IMAGE} !
 gstTest "--gst-plugin-path=${PATH_TO_PLUGIN} filesrc location=${PATH_TO_IMAGE} ! pngdec ! videoscale ! imagefreeze ! videoconvert ! video/x-raw,format=RGB,framerate=0/1 ! tensor_converter ! tensor_filter framework=tensorflow-lite model=${PATH_TO_MODEL} output=1:7 outputtype=int8 ! filesink location=tensorfilter.out.log" 3F_n 0 1 $PERFORMANCE
 
 # Property reading test for nnapi
-gstTest "--gst-plugin-path=${PATH_TO_PLUGIN} filesrc location=${PATH_TO_IMAGE} ! pngdec ! videoscale ! imagefreeze ! videoconvert ! video/x-raw,format=RGB,framerate=0/1 ! tensor_converter ! tensor_filter framework=tensorflow-lite model=${PATH_TO_MODEL} nnapi=true:cpu ! filesink location=tensorfilter.out.log" 2-1 1 0 $PERFORMANCE 2> info
-# This test is not critical because NNAPI is not available in some systems (it's available with Tizen-ARM only!)
-if [ "$SYSTEM" == "armv7l" ] || [ "$SYSTEM" == "aarch64" ]; then
-	cat info | grep "true : cpu"
-	testResult $? 2-1a "NNAPI activaion test" 1 1
-fi
+gst-launch-1.0 --gst-plugin-path=${PATH_TO_PLUGIN} filesrc location=${PATH_TO_IMAGE} ! pngdec ! videoscale ! imagefreeze ! videoconvert ! video/x-raw,format=RGB,framerate=0/1 ! tensor_converter ! tensor_filter framework=tensorflow-lite model=${PATH_TO_MODEL} nnapi=true:cpu ! filesink location=tensorfilter.out.log 2> info
+
+cat info | grep "true : cpu"
+testResult $? 2-1 "NNAPI activaion test" 1 1
 
 # Property reading test for nnapi
-gstTest "--gst-plugin-path=${PATH_TO_PLUGIN} filesrc location=${PATH_TO_IMAGE} ! pngdec ! videoscale ! imagefreeze ! videoconvert ! video/x-raw,format=RGB,framerate=0/1 ! tensor_converter ! tensor_filter framework=tensorflow-lite model=${PATH_TO_MODEL} nnapi=true ! filesink location=tensorfilter.out.log" 2-2 1 0 $PERFORMANCE 2> info
-# This test is not critical because NNAPI is not available in some systems (it's available with Tizen-ARM only!)
-if [ "$SYSTEM" == "armv7l" ] || [ "$SYSTEM" == "aarch64" ]; then
-	cat info | grep "true : cpu"
-	testResult $? 2-2a "NNAPI activation test" 1 1
-fi
+gst-launch-1.0 --gst-plugin-path=${PATH_TO_PLUGIN} filesrc location=${PATH_TO_IMAGE} ! pngdec ! videoscale ! imagefreeze ! videoconvert ! video/x-raw,format=RGB,framerate=0/1 ! tensor_converter ! tensor_filter framework=tensorflow-lite model=${PATH_TO_MODEL} nnapi=true ! filesink location=tensorfilter.out.log 2> info
+
+cat info | grep "true : cpu"
+testResult $? 2-2 "NNAPI activation test" 1 1
 
 # Property reading test for nnapi
-gstTest "--gst-plugin-path=${PATH_TO_PLUGIN} filesrc location=${PATH_TO_IMAGE} ! pngdec ! videoscale ! imagefreeze ! videoconvert ! video/x-raw,format=RGB,framerate=0/1 ! tensor_converter ! tensor_filter framework=tensorflow-lite model=${PATH_TO_MODEL} nnapi=true:gpu ! filesink location=tensorfilter.out.log" 2-3 1 0 $PERFORMANCE 2> info
-# This test is not critical because NNAPI is not available in some systems (it's available with Tizen-ARM only!)
-if [ "$SYSTEM" == "armv7l" ] || [ "$SYSTEM" == "aarch64" ]; then
-	cat info | grep "true : gpu"
-	testResult $? 2-3a "NNAPI activation test" 1 1
-	# This test is not critical because NNAPI is not available in some systems (it's available with Tizen-ARM only!)
-fi
+gst-launch-1.0 --gst-plugin-path=${PATH_TO_PLUGIN} filesrc location=${PATH_TO_IMAGE} ! pngdec ! videoscale ! imagefreeze ! videoconvert ! video/x-raw,format=RGB,framerate=0/1 ! tensor_converter ! tensor_filter framework=tensorflow-lite model=${PATH_TO_MODEL} nnapi=true:gpu ! filesink location=tensorfilter.out.log 2> info
+
+cat info | grep "true : gpu"
+testResult $? 2-3 "NNAPI activation test" 1 1
 
 report
