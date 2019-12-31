80154edc4fcab0c473b2f722c9a4ba2887b257c0
Parichay Kapoor <pk.kapoor@samsung.com>
2019-12-31 16:39:24 +0900

    [armnn] object classification unittest + bug solve
    - Add object classification unittest using tflite quantized mobilenet
    - Add orange.png raw format image orange.raw
    - As armnn treats uint8 datatype of tflite as quant asymm8 (arc/armnnTfLiteParser/TfLiteParser.cpp:310),
    quant asymm8 is interpreted as uint8 in nnstreamer
    - Updated current default HW backend to CpuAcc (based on neon) as CpuRef takes over
    5 min to complete the mobilenet unittest
    
    Bug solve:
    The dimension of data received by armnn are inversed to match the desired order
    
    Signed-off-by: Parichay Kapoor <pk.kapoor@samsung.com>

--- tests/nnstreamer_filter_armnn/unittest_filter_armnn.cpp
+++ tests/nnstreamer_filter_armnn/unittest_filter_armnn.cpp
@@ -10,7 +10,11 @@
 #include <gtest/gtest.h>
 #include <glib.h>
 #include <glib/gstdio.h>        /* GStatBuf */
+#include <sys/stat.h>
+#include <fcntl.h>
+
 #include <nnstreamer_plugin_api_filter.h>
+#include <nnstreamer_plugin_api.h>
 
 /**
  * @brief Test armnn subplugin existence.
@@ -156,7 +160,7 @@ TEST (nnstreamer_filter_armnn, get_dimension)
 }
 
 /**
- * @brief Test armnn subplugin with successful invoke
+ * @brief Test armnn subplugin with successful invoke for tflite
  */
 TEST (nnstreamer_filter_armnn, invoke_basic)
 {
@@ -217,6 +221,134 @@ TEST (nnstreamer_filter_armnn, invoke_basic)
 
   g_free (input.data);
   g_free (output.data);
+  sp->close (&prop, &data);
+}
+
+/**
+ * @brief get argmax from the array
+ */
+size_t get_argmax (guint8 *array, size_t size)
+{
+  size_t idx, max_idx = 0;
+  guint8 max_value = 0;
+  for (idx = 0; idx < size; idx ++) {
+    if (max_value < array[idx]) {
+      max_idx = idx;
+      max_value = array[idx];
+    }
+  }
+
+  return max_idx;
+}
+
+/**
+ * @brief Test armnn subplugin with successful invoke for tflite advanced model
+ */
+TEST (nnstreamer_filter_armnn, invoke_advanced)
+{
+  int ret, fd;
+  void *data = NULL;
+  gchar *model_file;
+  const gchar *root_path = g_getenv ("NNSTREAMER_BUILD_ROOT_PATH");
+  GstTensorMemory input, output;
+  GstTensorsInfo info, res;
+  char *data_file;
+  ssize_t data_read;
+  size_t max_idx;
+  gboolean status;
+
+  ASSERT_NE (root_path, nullptr);
+
+  /** armnn needs a directory with model file and metadata in that directory */
+  model_file = g_build_filename (root_path, "tests", "test_models", "models",
+      "mobilenet_v1_1.0_224_quant.tflite", NULL);
+  status = g_file_test (model_file, G_FILE_TEST_EXISTS);
+  if (status == FALSE) {
+    g_free (model_file);
+    ASSERT_EQ (status, TRUE);
+  }
+
+  const gchar *model_files[] = { model_file, NULL, };
+  GstTensorFilterProperties prop = {
+    .fwname = "armnn",
+    .fw_opened = 0,
+    .model_files = model_files,
+    .num_models = 1,
+  };
+
+  const GstTensorFilterFramework *sp = nnstreamer_filter_find ("armnn");
+  EXPECT_NE (sp, (void *) NULL);
+
+  info.num_tensors = 1;
+  info.info[0].type = _NNS_UINT8;
+  info.info[0].dimension[0] = 3;
+  info.info[0].dimension[1] = 224;
+  info.info[0].dimension[2] = 224;
+  info.info[0].dimension[3] = 1;
+
+  ret = sp->open (&prop, &data);
+  EXPECT_EQ (ret, 0);
+
+  /** get input/output dimension successfully */
+  ret = sp->getInputDimension (&prop, &data, &res);
+  EXPECT_EQ (ret, 0);
+
+  EXPECT_EQ (res.num_tensors, info.num_tensors);
+  EXPECT_EQ (res.info[0].type, info.info[0].type);
+  EXPECT_EQ (res.info[0].dimension[0], info.info[0].dimension[0]);
+  EXPECT_EQ (res.info[0].dimension[1], info.info[0].dimension[1]);
+  EXPECT_EQ (res.info[0].dimension[2], info.info[0].dimension[2]);
+  EXPECT_EQ (res.info[0].dimension[3], info.info[0].dimension[3]);
+
+  input.type = res.info[0].type;
+  input.size = gst_tensor_info_get_size (&res.info[0]);
+
+  ret = sp->getOutputDimension (&prop, &data, &res);
+  EXPECT_EQ (ret, 0);
+
+  info.num_tensors = 1;
+  info.info[0].type = _NNS_UINT8;
+  info.info[0].dimension[0] = 1001;
+  info.info[0].dimension[1] = 1;
+  info.info[0].dimension[2] = 1;
+  info.info[0].dimension[3] = 1;
+
+  EXPECT_EQ (res.num_tensors, info.num_tensors);
+  EXPECT_EQ (res.info[0].type, info.info[0].type);
+  EXPECT_EQ (res.info[0].dimension[0], info.info[0].dimension[0]);
+  EXPECT_EQ (res.info[0].dimension[1], info.info[0].dimension[1]);
+  EXPECT_EQ (res.info[0].dimension[2], info.info[0].dimension[2]);
+  EXPECT_EQ (res.info[0].dimension[3], info.info[0].dimension[3]);
+
+  output.type = res.info[0].type;
+  output.size = gst_tensor_info_get_size (&res.info[0]);
+
+  input.data = g_malloc(input.size);
+  output.data = g_malloc(output.size);
+
+  data_file = g_build_filename (root_path, "tests", "test_models", "data",
+      "orange.raw", NULL);
+  fd = open(data_file, O_RDONLY);
+
+  EXPECT_TRUE (fd >= 0);
+  /** Invoke output will not match with fd < 0 - input will be random data */
+  if (fd >= 0) {
+    data_read = read(fd, input.data, input.size);
+    EXPECT_EQ (data_read, input.size);
+    close(fd);
+  }
+
+  ret = sp->invoke_NN (&prop, &data, &input, &output);
+  EXPECT_EQ (ret, 0);
+
+  /** entry 952 (idx 951) is orange as per tests/test_models/labels/labels.txt */
+  max_idx = get_argmax ((guint8 *)output.data, output.size);
+  EXPECT_EQ (max_idx, 951);
+
+  g_free (data_file);
+  g_free (output.data);
+  g_free (input.data);
+  g_free (model_file);
   sp->close (&prop, &data);
 }
 
