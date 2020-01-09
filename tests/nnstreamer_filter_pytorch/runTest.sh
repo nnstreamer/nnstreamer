032cd69a61cf9af00100d75d7066521eed1b9431
Parichay Kapoor <pk.kapoor@samsung.com>
2020-01-10 07:57:46 +0900

    [pytorch] Enable setting accelerators
    Enabled setting of accelerators
    Enabled getting supported accelerators
    
    Signed-off-by: Parichay Kapoor <pk.kapoor@samsung.com>

--- tests/nnstreamer_filter_pytorch/runTest.sh
+++ tests/nnstreamer_filter_pytorch/runTest.sh
@@ -76,4 +76,16 @@ gstTest "--gst-plugin-path=${PATH_TO_PLUGIN} filesrc location=${PATH_TO_IMAGE} !
 # Fail test for invalid output properties
 gstTest "--gst-plugin-path=${PATH_TO_PLUGIN} filesrc location=${PATH_TO_IMAGE} ! pngdec ! videoscale ! imagefreeze ! videoconvert ! video/x-raw,format=GRAY8,framerate=0/1 ! tensor_converter ! tensor_filter framework=pytorch model=${PATH_TO_MODEL} output=1:7 outputtype=int8 ! filesink location=tensorfilter.out.log" 3F_n 0 1 $PERFORMANCE
 
+# Test the setting of accelerators
+gst-launch-1.0 --gst-plugin-path=${PATH_TO_PLUGIN} filesrc location=${PATH_TO_IMAGE} ! pngdec ! videoscale ! imagefreeze ! videoconvert ! video/x-raw,format=GRAY8,framerate=0/1 ! tensor_converter ! tensor_filter framework=pytorch model=${PATH_TO_MODEL} output=1:7 outputtype=int8 accelerator=true:gpu ! filesink location=tensorfilter.out.log 2>info
+cat info | grep "gpu = 1, accl = gpu"
+testResult $? 2-1 "GPU activation test" 0 1
+
+gst-launch-1.0 --gst-plugin-path=${PATH_TO_PLUGIN} filesrc location=${PATH_TO_IMAGE} ! pngdec ! videoscale ! imagefreeze ! videoconvert ! video/x-raw,format=GRAY8,framerate=0/1 ! tensor_converter ! tensor_filter framework=pytorch model=${PATH_TO_MODEL} output=1:7 outputtype=int8 accelerator=true:cpu ! filesink location=tensorfilter.out.log 2>info
+cat info | grep "gpu = 0, accl = cpu"
+testResult $? 2-2 "GPU activation test" 0 1
+
+gst-launch-1.0 --gst-plugin-path=${PATH_TO_PLUGIN} filesrc location=${PATH_TO_IMAGE} ! pngdec ! videoscale ! imagefreeze ! videoconvert ! video/x-raw,format=GRAY8,framerate=0/1 ! tensor_converter ! tensor_filter framework=pytorch model=${PATH_TO_MODEL} output=1:7 outputtype=int8 ! filesink location=tensorfilter.out.log 2>info
+cat info | grep "gpu = 0, accl = default"
+testResult $? 2-3 "GPU activation test" 0 1
 report
