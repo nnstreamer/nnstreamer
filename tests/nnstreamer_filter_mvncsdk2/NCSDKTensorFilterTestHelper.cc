f460acb11dd976ae2e486948d912775917bf0358
Wook Song <wook16.song@samsung.com>
2020-01-28 08:12:01 +0900

    [Tests/Filter/MvNCSDK2] Add a helper class for unit testing
    This patch adds a helper class for unit testing of the mvncsdk2 tensor
    filter.
    
    Signed-off-by: Wook Song <wook16.song@samsung.com>

---
+++ tests/nnstreamer_filter_mvncsdk2/NCSDKTensorFilterTestHelper.cc
@@ -0,0 +1,566 @@
+/**
+ * @file NCSDKTensorFilterTestHelper.cc
+ * @date 8 Jan 2020
+ * @author  Wook Song <wook16.song@samsung.com>
+ * @brief Helper class for testing tensor_filter_mvncsdk2 without an actual device
+ * @see https://github.com/nnsuite/nnstreamer
+ * @bug	No known bugs except for NYI items
+ *
+ *  Copyright 2020 Samsung Electronics
+ *
+ */
+
+#include "NCSDKTensorFilterTestHelper.hh"
+
+// Static member variables for instance management
+std::unique_ptr<NCSDKTensorFilterTestHelper>
+    NCSDKTensorFilterTestHelper::mInstance;
+std::once_flag NCSDKTensorFilterTestHelper::mOnceFlag;
+
+/**
+ * @brief Default constuctor. Note that explicit invocation of init () is always
+ * required after getting the instance.
+ */
+NCSDKTensorFilterTestHelper::NCSDKTensorFilterTestHelper ()
+{
+  /* Do nothing */
+}
+
+/**
+ * @brief Destructor.Resources are deallocated.
+ */
+NCSDKTensorFilterTestHelper::~NCSDKTensorFilterTestHelper ()
+{
+  /* Do nothing */
+}
+
+/**
+ * @brief A method to initialize this helper class
+ * @param[in] model : A neural network model to simulate
+ */
+void
+NCSDKTensorFilterTestHelper::init (model_t model)
+{
+  // MAJOR should be 2
+  this->mVer[MAJOR] = 2;
+  // Don't care other indexes, MINOR, HOTFIX, and RC
+  this->mVer[MINOR] = 3;
+  this->mVer[HOTFIX] = 4;
+  this->mVer[RC] = 5;
+
+  try {
+    this->mDevHandle = new ncDeviceHandle_t;
+  } catch (const std::bad_alloc & e) {
+    this->mDevHandle = nullptr;
+  }
+
+  try {
+    this->mGraphHandle = new ncGraphHandle_t;
+  } catch (const std::bad_alloc & e) {
+    this->mGraphHandle = nullptr;
+  }
+  this->mModelPath = nullptr;
+
+  switch (model) {
+    default:
+    case GOOGLE_LENET:
+      this->mModel = model;
+      try {
+        this->mTensorDescInput = new struct ncTensorDescriptor_t ();
+        this->mTensorDescInput->c = GOOGLE_LENET_IN_DIM_C;
+        this->mTensorDescInput->n = GOOGLE_LENET_IN_DIM_N;
+        this->mTensorDescInput->w = GOOGLE_LENET_IN_DIM_W;
+        this->mTensorDescInput->h = GOOGLE_LENET_IN_DIM_H;
+        this->mTensorDescInput->dataType = NC_FIFO_FP32;
+
+        this->mTensorDescOutput = new struct ncTensorDescriptor_t ();
+        this->mTensorDescOutput->c = GOOGLE_LENET_OUT_DIM_C;
+        this->mTensorDescOutput->n = GOOGLE_LENET_OUT_DIM_N;
+        this->mTensorDescOutput->w = GOOGLE_LENET_OUT_DIM_W;
+        this->mTensorDescOutput->h = GOOGLE_LENET_OUT_DIM_H;
+        this->mTensorDescOutput->dataType = NC_FIFO_FP32;
+      } catch  (const std::bad_alloc & e) {
+        this->mTensorDescInput = nullptr;
+        this->mTensorDescOutput = nullptr;
+      }
+      break;
+  }
+}
+
+/**
+ * @brief A method to release allocated resources
+ */
+void
+NCSDKTensorFilterTestHelper::release ()
+{
+  this->ncDeviceDestroy (&(this->mDevHandle));
+  this->ncGraphDestroy (&(this->mGraphHandle));
+
+  if (this->mTensorDescInput != nullptr) {
+    delete this->mTensorDescInput;
+    this->mTensorDescInput = nullptr;
+  }
+
+  if (this->mTensorDescOutput != nullptr) {
+    delete this->mTensorDescOutput;
+    this->mTensorDescOutput = nullptr;
+  }
+
+  g_free (this->mModelPath);
+}
+
+/**
+ * @brief A method mocking ncGlobalGetOption()
+ */
+ncStatus_t
+NCSDKTensorFilterTestHelper::ncGlobalGetOption (int option, void *data,
+      unsigned int *dataLength)
+{
+  switch (option) {
+    case NC_RO_API_VERSION:
+      if (sizeof(ncsdk_ver_t) != (*dataLength))
+        return NC_ERROR;
+      memcpy (data, this->mVer, *dataLength);
+      break;
+    default:
+      return NC_ERROR;
+  }
+  return NC_OK;
+}
+
+/**
+ * @brief A method mocking ncDeviceCreate()
+ */
+ncStatus_t
+NCSDKTensorFilterTestHelper::ncDeviceCreate (int index,
+    struct ncDeviceHandle_t **deviceHandle)
+{
+  if ((index < 0) || (index >= SUPPORT_MAX_NUMS_DEVICES))
+    return NC_INVALID_PARAMETERS;
+
+  if (this->mDevHandle == nullptr)
+    return NC_OUT_OF_MEMORY;
+
+  *deviceHandle = this->mDevHandle;
+
+  return NC_OK;
+}
+
+/**
+ * @brief A method mocking ncDeviceOpen()
+ */
+ncStatus_t
+NCSDKTensorFilterTestHelper::ncDeviceOpen(struct ncDeviceHandle_t *deviceHandle)
+{
+  if (deviceHandle != this->mDevHandle)
+    return NC_NOT_ALLOCATED;
+
+  return NC_OK;
+}
+
+/**
+ * @brief A method mocking ncDeviceClose()
+ */
+ncStatus_t
+NCSDKTensorFilterTestHelper::ncDeviceClose(
+    struct ncDeviceHandle_t *deviceHandle)
+{
+  if (deviceHandle != this->mDevHandle)
+    return NC_NOT_ALLOCATED;
+
+  return NC_OK;
+}
+
+/**
+ * @brief A method mocking ncDeviceDestroy()
+ */
+ncStatus_t
+NCSDKTensorFilterTestHelper::ncDeviceDestroy(
+    struct ncDeviceHandle_t **deviceHandle)
+{
+  if ((this->mDevHandle != nullptr) && (*deviceHandle != nullptr)
+      && (*deviceHandle == this->mDevHandle)) {
+    delete this->mDevHandle;
+    this->mDevHandle = nullptr;
+    *deviceHandle = nullptr;
+  } else {
+    return NC_NOT_ALLOCATED;
+  }
+
+  return NC_OK;
+}
+
+/**
+ * @brief A method mocking ncGraphCreate()
+ */
+ncStatus_t
+NCSDKTensorFilterTestHelper::ncGraphCreate(const char* name,
+    struct ncGraphHandle_t **graphHandle)
+{
+  if (!g_file_test (name, G_FILE_TEST_IS_REGULAR)) {
+    return NC_INVALID_PARAMETERS;
+  }
+
+  if (this->mGraphHandle == nullptr) {
+    return NC_OUT_OF_MEMORY;
+  }
+
+  this->mModelPath = g_strdup (name);
+  *graphHandle = this->mGraphHandle;
+
+  return NC_OK;
+}
+
+/**
+ * @brief A method mocking ncGraphAllocate()
+ */
+ncStatus_t
+NCSDKTensorFilterTestHelper::ncGraphAllocate(
+    struct ncDeviceHandle_t *deviceHandle, struct ncGraphHandle_t *graphHandle,
+    const void *graphBuffer, unsigned int graphBufferLength)
+{
+  if ((this->mDevHandle != deviceHandle) || (this->mGraphHandle != graphHandle))
+    return NC_INVALID_PARAMETERS;
+
+  this->mGraphBuf = graphBuffer;
+  this->mLenGraphBuf = graphBufferLength;
+
+  return NC_OK;
+}
+
+/**
+ * @brief A method mocking ncGraphGetOption().
+ * A description of input or output tensor will be filled in data.
+ */
+ncStatus_t
+NCSDKTensorFilterTestHelper::ncGraphGetOption(
+    struct ncGraphHandle_t *graphHandle, int option, void *data,
+    unsigned int *dataLength)
+{
+  switch (option) {
+    case NC_RO_GRAPH_INPUT_TENSOR_DESCRIPTORS:
+      if (this->mTensorDescInput == nullptr) {
+        return NC_OUT_OF_MEMORY;
+      }
+      if (sizeof(*this->mTensorDescInput) != (*dataLength))
+        return NC_INVALID_PARAMETERS;
+      memcpy (data, (void *) this->mTensorDescInput, *dataLength);
+      break;
+    case NC_RO_GRAPH_OUTPUT_TENSOR_DESCRIPTORS:
+      if (this->mTensorDescOutput == nullptr) {
+        return NC_OUT_OF_MEMORY;
+      }
+      if (sizeof(*this->mTensorDescOutput) != (*dataLength))
+        return NC_INVALID_PARAMETERS;
+      memcpy (data, (void *) this->mTensorDescOutput, *dataLength);
+      break;
+  }
+
+  return NC_OK;
+}
+
+/**
+ * @brief A method mocking ncGraphQueueInference(). Do nothing.
+ */
+ncStatus_t
+NCSDKTensorFilterTestHelper::ncGraphQueueInference(
+    struct ncGraphHandle_t *graphHandle, struct ncFifoHandle_t** fifoIn,
+    unsigned int inFifoCount, struct ncFifoHandle_t** fifoOut,
+    unsigned int outFifoCount)
+{
+  return NC_OK;
+}
+
+/**
+ * @brief A method mocking ncGraphQueueInference(). As the destructor does,
+ * this deallocates the resources allocated by ncGraphCreate()
+ */
+ncStatus_t
+NCSDKTensorFilterTestHelper::ncGraphDestroy(
+  struct ncGraphHandle_t **graphHandle)
+{
+  if ((*graphHandle == nullptr) || (*graphHandle != this->mGraphHandle)) {
+    return NC_INVALID_PARAMETERS;
+  }
+
+  if (this->mModelPath != nullptr) {
+    g_free (this->mModelPath);
+    this->mModelPath = nullptr;
+  }
+
+  if (this->mGraphHandle != nullptr) {
+    delete this->mGraphHandle;
+    this->mGraphHandle = nullptr;
+  }
+
+  return NC_OK;
+}
+
+/**
+ * @brief A method mocking ncFifoCreate(). Do nothing.
+ */
+ncStatus_t
+NCSDKTensorFilterTestHelper::ncFifoCreate(const char* name, ncFifoType_t type,
+    struct ncFifoHandle_t** fifoHandle)
+{
+  return NC_OK;
+}
+
+/**
+ * @brief A method mocking ncFifoAllocate(). Do nothing.
+ */
+ncStatus_t
+NCSDKTensorFilterTestHelper::ncFifoAllocate(struct ncFifoHandle_t* fifoHandle,
+    struct ncDeviceHandle_t* device, struct ncTensorDescriptor_t* tensorDesc,
+    unsigned int numElem)
+{
+  return NC_OK;
+}
+
+/**
+ * @brief A method mocking ncFifoSetOption(). Do nothing.
+ */
+ncStatus_t
+NCSDKTensorFilterTestHelper::ncFifoSetOption(struct ncFifoHandle_t* fifoHandle,
+    int option, const void *data, unsigned int dataLength)
+{
+  return NC_OK;
+}
+
+/**
+ * @brief A method mocking ncFifoGetOption(). Do nothing.
+ */
+ncStatus_t
+NCSDKTensorFilterTestHelper::ncFifoGetOption(struct ncFifoHandle_t* fifoHandle,
+    int option, void *data, unsigned int *dataLength)
+{
+  return NC_OK;
+}
+
+/**
+ * @brief A method mocking ncFifoDestroy(). Do nothing.
+ */
+ncStatus_t
+NCSDKTensorFilterTestHelper::ncFifoDestroy(struct ncFifoHandle_t** fifoHandle)
+{
+  return NC_OK;
+}
+
+/**
+ * @brief A method mocking ncFifoWriteElem(). Do nothing.
+ */
+ncStatus_t
+NCSDKTensorFilterTestHelper::ncFifoWriteElem(struct ncFifoHandle_t* fifoHandle,
+    const void *inputTensor, unsigned int * inputTensorLength, void *userParam)
+{
+  return NC_OK;
+}
+
+/**
+ * @brief A method mocking ncFifoReadElem(). Do nothing.
+ */
+ncStatus_t
+NCSDKTensorFilterTestHelper::ncFifoReadElem(struct ncFifoHandle_t* fifoHandle,
+    void *outputData, unsigned int* outputDataLen, void **userParam)
+{
+  return NC_OK;
+}
+
+/**
+ * @brief A method mocking ncFifoRemoveElem(). Do nothing.
+ */
+ncStatus_t
+NCSDKTensorFilterTestHelper::ncFifoRemoveElem(struct ncFifoHandle_t* fifoHandle)
+{
+  return NC_OK;
+}
+
+/**
+ * @brief A function definition providing the dummy body of ncGlobalGetOption() to the NCSDK2 tensor filter
+ */
+ncStatus_t
+ncGlobalGetOption (int option, void *data, unsigned int *dataLength)
+{
+  return NCSDKTensorFilterTestHelper::getInstance().ncGlobalGetOption (option,
+      data, dataLength);
+}
+
+/**
+ * @brief A function definition providing the dummy body of ncDeviceCreate() to the NCSDK2 tensor filter
+ */
+ncStatus_t
+ncDeviceCreate (int index, struct ncDeviceHandle_t **deviceHandle)
+{
+  return NCSDKTensorFilterTestHelper::getInstance().ncDeviceCreate (index,
+      deviceHandle);
+}
+
+/**
+ * @brief A function definition providing the dummy body of ncDeviceOpen() to the NCSDK2 tensor filter
+ */
+ncStatus_t
+ncDeviceOpen(struct ncDeviceHandle_t *deviceHandle)
+{
+  return NCSDKTensorFilterTestHelper::getInstance().ncDeviceOpen (deviceHandle);
+}
+
+/**
+ * @brief A function definition providing the dummy body of ncDeviceClose() to the NCSDK2 tensor filter
+ */
+ncStatus_t
+ncDeviceClose(struct ncDeviceHandle_t *deviceHandle)
+{
+  return NCSDKTensorFilterTestHelper::getInstance()
+      .ncDeviceClose (deviceHandle);
+}
+
+/**
+ * @brief A function definition providing the dummy body of ncDeviceDestroy() to the NCSDK2 tensor filter
+ */
+ncStatus_t
+ncDeviceDestroy(struct ncDeviceHandle_t **deviceHandle)
+{
+  return NCSDKTensorFilterTestHelper::getInstance()
+      .ncDeviceDestroy (deviceHandle);
+}
+
+/**
+ * @brief A function definition providing the dummy body of ncGraphCreate() to the NCSDK2 tensor filter
+ */
+ncStatus_t
+ncGraphCreate (const char* name, struct ncGraphHandle_t **graphHandle)
+{
+  return NCSDKTensorFilterTestHelper::getInstance().ncGraphCreate (name,
+      graphHandle);
+}
+
+/**
+ * @brief A function definition providing the dummy body of ncGraphAllocate() to the NCSDK2 tensor filter
+ */
+ncStatus_t
+ncGraphAllocate(struct ncDeviceHandle_t *deviceHandle,
+      struct ncGraphHandle_t *graphHandle, const void *graphBuffer,
+      unsigned int graphBufferLength)
+{
+  return NCSDKTensorFilterTestHelper::getInstance().ncGraphAllocate (
+      deviceHandle, graphHandle, graphBuffer, graphBufferLength);
+}
+
+/**
+ * @brief A function definition providing the dummy body of ncGraphGetOption() to the NCSDK2 tensor filter
+ */
+ncStatus_t
+ncGraphGetOption(struct ncGraphHandle_t *graphHandle, int option, void *data,
+    unsigned int *dataLength)
+{
+  return NCSDKTensorFilterTestHelper::getInstance().ncGraphGetOption (
+      graphHandle, option, data, dataLength);
+}
+
+/**
+ * @brief A function definition providing the dummy body of ncGraphQueueInference() to the NCSDK2 tensor filter
+ */
+ncStatus_t
+ncGraphQueueInference(struct ncGraphHandle_t *graphHandle,
+    struct ncFifoHandle_t** fifoIn, unsigned int inFifoCount,
+    struct ncFifoHandle_t** fifoOut, unsigned int outFifoCount)
+{
+  return NCSDKTensorFilterTestHelper::getInstance().ncGraphQueueInference (
+      graphHandle, fifoIn, inFifoCount, fifoOut, outFifoCount);
+}
+
+/**
+ * @brief A function definition providing the dummy body of ncGraphDestroy() to the NCSDK2 tensor filter
+ */
+ncStatus_t
+ncGraphDestroy(struct ncGraphHandle_t **graphHandle)
+{
+  return NCSDKTensorFilterTestHelper::getInstance()
+      .ncGraphDestroy (graphHandle);
+}
+
+/**
+ * @brief A function definition providing the dummy body of ncFifoCreate() to the NCSDK2 tensor filter
+ */
+ncStatus_t
+ncFifoCreate(const char* name, ncFifoType_t type,
+    struct ncFifoHandle_t** fifoHandle)
+{
+  return NCSDKTensorFilterTestHelper::getInstance().ncFifoCreate (name, type,
+      fifoHandle);
+}
+
+/**
+ * @brief A function definition providing the dummy body of ncFifoAllocate() to the NCSDK2 tensor filter
+ */
+ncStatus_t
+ncFifoAllocate(struct ncFifoHandle_t* fifoHandle,
+    struct ncDeviceHandle_t* device, struct ncTensorDescriptor_t* tensorDesc,
+    unsigned int numElem)
+{
+  return NCSDKTensorFilterTestHelper::getInstance().ncFifoAllocate (fifoHandle,
+      device, tensorDesc, numElem);
+}
+
+/**
+ * @brief A function definition providing the dummy body of ncFifoSetOption() to the NCSDK2 tensor filter
+ */
+ncStatus_t
+ncFifoSetOption(struct ncFifoHandle_t* fifoHandle, int option,
+    const void *data, unsigned int dataLength)
+{
+  return NCSDKTensorFilterTestHelper::getInstance().ncFifoSetOption (fifoHandle,
+      option, data, dataLength);
+}
+
+/**
+ * @brief A function definition providing the dummy body of ncFifoGetOption() to the NCSDK2 tensor filter
+ */
+ncStatus_t
+ncFifoGetOption(struct ncFifoHandle_t* fifoHandle, int option, void *data,
+    unsigned int *dataLength)
+{
+  return NCSDKTensorFilterTestHelper::getInstance().ncFifoGetOption (fifoHandle,
+      option, data, dataLength);
+}
+
+/**
+ * @brief A function definition providing the dummy body of ncFifoDestroy() to the NCSDK2 tensor filter
+ */
+ncStatus_t
+ncFifoDestroy(struct ncFifoHandle_t** fifoHandle)
+{
+  return NCSDKTensorFilterTestHelper::getInstance().ncFifoDestroy (fifoHandle);
+}
+
+/**
+ * @brief A function definition providing the dummy body of ncFifoWriteElem() to the NCSDK2 tensor filter
+ */
+ncStatus_t
+ncFifoWriteElem(struct ncFifoHandle_t* fifoHandle, const void *inputTensor,
+    unsigned int * inputTensorLength, void *userParam)
+{
+  return NCSDKTensorFilterTestHelper::getInstance().ncFifoWriteElem (fifoHandle,
+      inputTensor, inputTensorLength, userParam);
+}
+
+/**
+ * @brief A function definition providing the dummy body of ncFifoReadElem() to the NCSDK2 tensor filter
+ */
+ncStatus_t
+ncFifoReadElem(struct ncFifoHandle_t* fifoHandle, void *outputData,
+    unsigned int* outputDataLen, void **userParam)
+{
+  return NCSDKTensorFilterTestHelper::getInstance().ncFifoWriteElem (fifoHandle,
+      outputData, outputDataLen, userParam);
+}
+
+/**
+ * @brief A function definition providing the dummy body of ncFifoRemoveElem() to the NCSDK2 tensor filter
+ */
+ncStatus_t
+ncFifoRemoveElem(struct ncFifoHandle_t* fifoHandle)
+{
+  return NCSDKTensorFilterTestHelper::getInstance()
+      .ncFifoRemoveElem (fifoHandle);
+}
---
+++ tests/nnstreamer_filter_mvncsdk2/NCSDKTensorFilterTestHelper.hh
@@ -0,0 +1,125 @@
+/**
+ * @file NCSDKTensorFilterTestHelper.hh
+ * @date 8 Jan 2020
+ * @author  Wook Song <wook16.song@samsung.com>
+ * @brief Helper class for testing tensor_filter_mvncsdk2 without an actual device
+ * @see https://github.com/nnsuite/nnstreamer
+ * @bug	No known bugs except for NYI items
+ *
+ *  Copyright 2020 Samsung Electronics
+ *
+ */
+
+#include <glib.h>
+#include <gtest/gtest.h>
+#include <mvnc2/mvnc.h>
+#include <string.h>
+
+#include <mutex>
+
+enum _contants {
+  TENSOR_RANK_LIMIT = 4,
+  SUPPORT_MAX_NUMS_DEVICES = 8,
+};
+
+// Dimension inforamtion of Google LeNet
+enum _google_lenet {
+  GOOGLE_LENET_IN_DIM_C = 3,
+  GOOGLE_LENET_IN_DIM_W = 224,
+  GOOGLE_LENET_IN_DIM_H = 224,
+  GOOGLE_LENET_IN_DIM_N = 1,
+  GOOGLE_LENET_OUT_DIM_C = 1000,
+  GOOGLE_LENET_OUT_DIM_W = 1,
+  GOOGLE_LENET_OUT_DIM_H = 1,
+  GOOGLE_LENET_OUT_DIM_N = 1,
+};
+
+enum _ncsdk_ver_idx {
+  MAJOR = 0,
+  MINOR = 1,
+  HOTFIX= 2,
+  RC = 3,
+};
+
+typedef enum _model {
+  GOOGLE_LENET = 0,
+  DEFAULT_MODEL = GOOGLE_LENET,
+} model_t;
+
+typedef uint32_t ncsdk_ver_t[NC_VERSION_MAX_SIZE];
+
+class NCSDKTensorFilterTestHelper
+{
+public:
+  // Make this class as a singletone
+  static NCSDKTensorFilterTestHelper &getInstance () {
+    call_once (NCSDKTensorFilterTestHelper::mOnceFlag, []() {
+      mInstance.reset(new NCSDKTensorFilterTestHelper);
+    });
+    return *(mInstance.get ());
+  }
+  ~NCSDKTensorFilterTestHelper ();
+  void init (model_t model);
+  void release ();
+
+  // Mock methods that simulate NCSDK2 APIs
+  // Mock Global APIs
+  ncStatus_t ncGlobalGetOption (int option, void *data,
+      unsigned int *dataLength);
+  // Mock Device APIs
+  ncStatus_t ncDeviceCreate (int index,struct ncDeviceHandle_t **deviceHandle);
+  ncStatus_t ncDeviceOpen(struct ncDeviceHandle_t *deviceHandle);
+  ncStatus_t ncDeviceClose(struct ncDeviceHandle_t *deviceHandle);
+  ncStatus_t ncDeviceDestroy(struct ncDeviceHandle_t **deviceHandle);
+
+  // Mock Graph APIs
+  ncStatus_t ncGraphCreate(const char* name, struct ncGraphHandle_t **graphHandle);
+  ncStatus_t ncGraphAllocate(struct ncDeviceHandle_t *deviceHandle,
+      struct ncGraphHandle_t *graphHandle, const void *graphBuffer,
+      unsigned int graphBufferLength);
+  ncStatus_t ncGraphGetOption(struct ncGraphHandle_t *graphHandle,
+     int option, void *data, unsigned int *dataLength);
+  ncStatus_t ncGraphQueueInference(struct ncGraphHandle_t *graphHandle,
+      struct ncFifoHandle_t** fifoIn, unsigned int inFifoCount,
+      struct ncFifoHandle_t** fifoOut, unsigned int outFifoCount);
+  ncStatus_t ncGraphDestroy(struct ncGraphHandle_t **graphHandle);
+
+  // Mock FIFO APIs (returning only NC_OK)
+  ncStatus_t ncFifoCreate(const char* name, ncFifoType_t type,
+      struct ncFifoHandle_t** fifoHandle);
+  ncStatus_t ncFifoAllocate(struct ncFifoHandle_t* fifoHandle,
+      struct ncDeviceHandle_t* device, struct ncTensorDescriptor_t* tensorDesc,
+      unsigned int numElem);
+  ncStatus_t ncFifoSetOption(struct ncFifoHandle_t* fifoHandle, int option,
+      const void *data, unsigned int dataLength);
+  ncStatus_t ncFifoGetOption(struct ncFifoHandle_t* fifoHandle, int option,
+      void *data, unsigned int *dataLength);
+  ncStatus_t ncFifoDestroy(struct ncFifoHandle_t** fifoHandle);
+  ncStatus_t ncFifoWriteElem(struct ncFifoHandle_t* fifoHandle,
+      const void *inputTensor, unsigned int * inputTensorLength,
+      void *userParam);
+  ncStatus_t ncFifoReadElem(struct ncFifoHandle_t* fifoHandle, void *outputData,
+      unsigned int* outputDataLen, void **userParam);
+  ncStatus_t ncFifoRemoveElem(struct ncFifoHandle_t* fifoHandle); //not supported yet
+
+private:
+  // Variables for instance mangement
+  static std::unique_ptr<NCSDKTensorFilterTestHelper> mInstance;
+  static std::once_flag mOnceFlag;
+
+  // Constructor and destructor
+  NCSDKTensorFilterTestHelper ();
+  NCSDKTensorFilterTestHelper (const NCSDKTensorFilterTestHelper &) = delete;
+  NCSDKTensorFilterTestHelper &operator=(const NCSDKTensorFilterTestHelper &)
+      = delete;
+
+  struct ncDeviceHandle_t * mDevHandle;
+  struct ncGraphHandle_t * mGraphHandle;
+  struct ncTensorDescriptor_t * mTensorDescInput;
+  struct ncTensorDescriptor_t * mTensorDescOutput;
+  const void *mGraphBuf;
+  uint32_t mLenGraphBuf;
+  ncsdk_ver_t mVer;
+  gchar *mModelPath;
+  model_t mModel;
+};
