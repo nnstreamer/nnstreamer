febe3b3267070aca90717994e18b96a82acb4aac
Parichay Kapoor <pk.kapoor@samsung.com>
2020-02-06 13:50:41 +0900

    [tests] Clean up files after tests
    Clean up files created during tests with ssat
    
    Signed-off-by: Parichay Kapoor <pk.kapoor@samsung.com>

--- tests/nnstreamer_filter_pytorch/runTest.sh
+++ tests/nnstreamer_filter_pytorch/runTest.sh
@@ -88,4 +88,8 @@ testResult $? 2-2 "GPU activation test" 0 1
 gst-launch-1.0 --gst-plugin-path=${PATH_TO_PLUGIN} filesrc location=${PATH_TO_IMAGE} ! pngdec ! videoscale ! imagefreeze ! videoconvert ! video/x-raw,format=GRAY8,framerate=0/1 ! tensor_converter ! tensor_filter framework=pytorch model=${PATH_TO_MODEL} output=1:7 outputtype=int8 ! filesink location=tensorfilter.out.log 2>info
 cat info | grep "gpu = 0, accl = default"
 testResult $? 2-3 "GPU activation test" 0 1
+
+# Cleanup
+rm info
+
 report
