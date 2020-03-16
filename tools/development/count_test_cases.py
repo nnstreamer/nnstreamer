902fda613b3e6ee8b5f66ba660232ddab0912a8f
MyungJoo Ham <myungjoo.ham@samsung.com>
2020-03-16 13:31:53 +0900

    [TestCase] Count negative cases of gtest results
    Count negative test cases of gtest results.
    SSAT negative test cases will be counted after SSAT updates.
    
    Addresses #2150
    Updates for SSAT will follow soon.
    
    Signed-off-by: MyungJoo Ham <myungjoo.ham@samsung.com>

--- tools/development/count_test_cases.py
+++ tools/development/count_test_cases.py
@@ -19,16 +19,24 @@ import os
 
 ## @brief read results of GTest's result xml file
 def readGtestXml(filename):
+    neg = 0
     try:
         with open(filename, "r") as f:
             r = f.readlines()
             for line in r:
+                res = re.match(r'\s*<testcase name="([^"]+_n)"', line)
+                if res:
+                    neg = neg + 1
+            f.seek(0)
+
+            r = f.readlines()
+            for line in r:
                 res = re.match(r'<testsuites tests="(\d+)" failures="(\d+)" disabled="(\d+)"', line)
                 if res:
-                    return (int(res.group(1)), int(res.group(1)) - int(res.group(2)) - int(res.group(3)), int(res.group(2)), int(res.group(3)))
+                    return (int(res.group(1)), int(res.group(1)) - int(res.group(2)) - int(res.group(3)), int(res.group(2)), int(res.group(3)), neg)
     except:
         print("No gtest results.")
-    return (0, 0, 0, 0)
+    return (0, 0, 0, 0, 0)
 
 ## @brief read results of SSAT summary file
 def readSSAT(filename):
@@ -54,20 +62,24 @@ def main():
     pg = 0
     fg = 0
     ig = 0
+    ng = 0
+    posg = 0
 
     for r, d, f in os.walk(sys.argv[1]):
         for file in f:
             if os.path.splitext(file)[1] == '.xml':
-                (t, p, f, i) = readGtestXml(os.path.join(r, file))
+                (t, p, f, i, n) = readGtestXml(os.path.join(r, file))
                 tg = tg + t
                 pg = pg + p
                 fg = fg + f
                 ig = ig + i
+                ng = ng + n
+    posg = pg + fg + ig - ng
 
     (t, p, f, i) = readSSAT(sys.argv[2])
 
     print("GTest (total " + str(tg) + " cases)")
-    print("  Passed: " + str(pg) + " / Failed: " + str(fg) + " / Ignored: " + str(ig))
+    print("  Passed: " + str(pg) + " / Failed: " + str(fg) + " / Ignored: " + str(ig) + " | Positive: " + str(posg) + " / Negative: " + str(ng))
     print("SSAT (total " + str(t) + " cases)")
     print("  Passed: " + str(p) + " / Failed: " + str(f) + " / Ignored: " + str(i))
     print("Grand Total: " + str(pg + t) + " cases")
