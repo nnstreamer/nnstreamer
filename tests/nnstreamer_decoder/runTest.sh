5af0a848fbfa2e1571e2aa96bd5184f0140cc21d
gichan-jang <gichan2.jang@samsung.com>
2020-07-06 16:47:34 +0900

    [Decoder] Dynamic update of tensor config
    Changed to update tensor config dynamically.
    Clear private data of subplugin, and init the subplugin.
    
    Signed-off-by: gichan-jang <gichan2.jang@samsung.com>

--- tests/nnstreamer_decoder/runTest.sh
+++ tests/nnstreamer_decoder/runTest.sh
@@ -58,4 +58,23 @@ callCompareTest testcase3.con.log testcase3.dec.log 3 "Compare for case 3" 0 0
 ## Trigger "decoder not configured"
 gstTest "--gst-plugin-path=${PATH_TO_PLUGIN} videotestsrc ! tensor_converter ! tensor_decoder mode=direct_video ! video/mpeg ! fakesink" 4F_n 0 1 $PERFORMANCE
 
+# Dynamic tensor config update test
+## After converting the flatbuf to the tensor, it is tested whether the config is updated in runtime.
+gstTest "--gst-plugin-path=${PATH_TO_PLUGIN} videotestsrc num-buffers=3 ! videoconvert ! video/x-raw, width=640, height=480, framerate=5/1,format=RGB ! \
+    tee name=t ! queue ! multifilesink location=\"test_05_raw_%1d.log\"
+    t. ! queue ! tensor_converter ! tensor_decoder mode=flatbuf ! other/flatbuf-tensor ! tensor_converter ! \
+        tensor_decoder mode=direct_video ! multifilesink location=\"test_05_decoded_%1d.log\" sync=true" 5 "Flatbuf dynamic tensor config update" 0 0 $PERFORMANCE
+callCompareTest test_05_raw_0.log test_05_decoded_0.log 5-1 "Compare for case 5-1" 1 0
+callCompareTest test_05_raw_1.log test_05_decoded_1.log 5-2 "Compare for case 5-2" 1 0
+callCompareTest test_05_raw_2.log test_05_decoded_2.log 5-3 "Compare for case 5-3" 1 0
+
+## After converting the protobuf to the tensor, it is tested whether the config is updated in runtime.
+gstTest "--gst-plugin-path=${PATH_TO_PLUGIN} videotestsrc num-buffers=3 ! videoconvert ! video/x-raw, width=640, height=480, framerate=5/1,format=RGB ! \
+    tee name=t ! queue ! multifilesink location=\"test_06_raw_%1d.log\"
+    t. ! queue ! tensor_converter ! tensor_decoder mode=protobuf ! other/protobuf-tensor ! tensor_converter ! \
+        tensor_decoder mode=direct_video ! multifilesink location=\"test_06_decoded_%1d.log\" sync=true" 6 "Protobuf dynamic tensor config update" 0 0 $PERFORMANCE
+callCompareTest test_06_raw_0.log test_06_decoded_0.log 6-1 "Compare for case 6-1" 1 0
+callCompareTest test_06_raw_1.log test_06_decoded_1.log 6-2 "Compare for case 6-2" 1 0
+callCompareTest test_06_raw_2.log test_06_decoded_2.log 6-3 "Compare for case 6-3" 1 0
+
 report
