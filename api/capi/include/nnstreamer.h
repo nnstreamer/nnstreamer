ed4e86d5041ea00e775d3dfc838975e6e63e1671
DaWoon Kim <kim1023123@gmail.com>
2020-08-10 21:31:24 +0900

    [C-api] Fix typo
    Fix typo from:
    nnstreamer.h
    
    Signed-off-by: DaWoon Kim <kim1023123@gmail.com>

--- api/capi/include/nnstreamer.h
+++ api/capi/include/nnstreamer.h
@@ -190,7 +190,7 @@ typedef enum {
   ML_PIPELINE_BUF_POLICY_AUTO_FREE      = 0, /**< Default. Application should not deallocate this buffer. NNStreamer will deallocate when the buffer is no more needed. */
   ML_PIPELINE_BUF_POLICY_DO_NOT_FREE    = 1, /**< This buffer is not to be freed by NNStreamer (i.e., it's a static object). However, be careful: NNStreamer might be accessing this object after the return of the API call. */
   ML_PIPELINE_BUF_POLICY_MAX,   /**< Max size of #ml_pipeline_buf_policy_e structure. */
-  ML_PIPELINE_BUF_SRC_EVENT_EOS         = 0x10000, /**< Trigger End-Of-Stream event for the corresponding appsrc and ignore the given input value. The correspnding appsrc will no longer accept new data after this. */
+  ML_PIPELINE_BUF_SRC_EVENT_EOS         = 0x10000, /**< Trigger End-Of-Stream event for the corresponding appsrc and ignore the given input value. The corresponding appsrc will no longer accept new data after this. */
 } ml_pipeline_buf_policy_e;
 
 /**
