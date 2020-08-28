61dd70d54c7cb187c321cf2099f02f94d35e415c
Geunsik Lim <geunsik.lim@samsung.com>
2020-08-28 13:45:29 +0900

    [TF2/Filter] Added a framework to support TF2-Lite (2.3.0)
    This commit is to support Tensorflow Lite 2.3.0 (TF2-Lite) additionally,
    so that we support Tensorflow 2.x based network models (e.g., ASR) in
    the Tizen 6.X platform.
    
    **Changelog**
    
      * Version 3:
        * Removed unnecessary tflite_nnapi delegation (@jaeyun-jung)
    
      * Version 2:
        * Added a debian packaging for TF-2.3.0 in tensorflow2 (review.tizen.org)
          * https://review.tizen.org/gerrit/#/c/243189/ (Reviewed, Merged)
          * https://launchpad.net/~nnstreamer/+archive/ubuntu/ppa-build-test (Published)
      * Added unit-test with ssat
    
      * Version 1:
        * Added tflite2_support feature for meson build procedure
        * Added TF2-Lite statements in the .spec file for Tizen
        * Generate two libraries with "-DTFLITE_VERSION" flag from
          the tensor_filter_tensorflow_lite.cc file
        * Added nnstreamer-tensorflow2-lite package
    
    Signed-off-by: Geunsik Lim <geunsik.lim@samsung.com>

---
+++ tests/nnstreamer_filter_tensorflow2_lite/checkLabel.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+##
+# SPDX-License-Identifier: LGPL-2.1-only
+#
+# Copyright (C) 2018 Samsung Electronics
+#
+# @file checkLabel.py
+# @brief Check the result label of tensorflow-lite model
+# @author HyoungJoo Ahn <hello.ahn@samsung.com>
+
+import sys
+import os
+import struct
+import string
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from gen24bBMP import convert_to_bytes
+
+
+def readbyte (filename):
+  F = open(filename, 'rb')
+  readbyte = F.read()
+  F.close()
+  return readbyte
+
+
+def readlabel (filename):
+  F = open(filename, 'r')
+  line = F.readlines()
+  F.close()
+  return line
+
+onehot = readbyte(sys.argv[1])
+onehot = [convert_to_bytes(x) for x in onehot]
+idx = onehot.index(max(onehot))
+
+label_list = readlabel(sys.argv[2])
+label = label_list[idx].strip()
+
+answer = sys.argv[3].strip()
+exit(label != answer)
