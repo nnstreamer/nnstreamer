b84702b1b72778e6a5eff33dd495411c3ed01d91
Yongjoo Ahn <yongjoo1.ahn@samsung.com>
2020-09-04 17:55:03 +0900

    [Android/SNPE] Modify SNPE prebuilt libs
    - We do not need to link all snpe prebuilt libs: only libSNPE requires to build aar
    - Move excluded libs to jniLibs
    
    Signed-off-by: Yongjoo Ahn <yongjoo1.ahn@samsung.com>

--- api/android/api/src/main/jni/Android-snpe-prebuilt.mk
+++ api/android/api/src/main/jni/Android-snpe-prebuilt.mk
@@ -20,27 +20,3 @@ LOCAL_MODULE := libSNPE
 LOCAL_SRC_FILES := $(SNPE_LIB_PATH)/libSNPE.so
 include $(PREBUILT_SHARED_LIBRARY)
 SNPE_PREBUILT_LIBS += libSNPE
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libsnpe_adsp
-LOCAL_SRC_FILES := $(SNPE_LIB_PATH)/libsnpe_adsp.so
-include $(PREBUILT_SHARED_LIBRARY)
-SNPE_PREBUILT_LIBS += libsnpe_adsp
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libsnpe_dsp_domains
-LOCAL_SRC_FILES := $(SNPE_LIB_PATH)/libsnpe_dsp_domains.so
-include $(PREBUILT_SHARED_LIBRARY)
-SNPE_PREBUILT_LIBS += libsnpe_dsp_domains
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libsnpe_dsp_domains_v2
-LOCAL_SRC_FILES := $(SNPE_LIB_PATH)/libsnpe_dsp_domains_v2.so
-include $(PREBUILT_SHARED_LIBRARY)
-SNPE_PREBUILT_LIBS += libsnpe_dsp_domains_v2
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libhta
-LOCAL_SRC_FILES := $(SNPE_LIB_PATH)/libhta.so
-include $(PREBUILT_SHARED_LIBRARY)
-SNPE_PREBUILT_LIBS += libhta
