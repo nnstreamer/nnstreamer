7c250be6c6f146a1fe05af82be213ff970e92fad
Jaeyun <jy1210.jung@samsung.com>
2020-10-23 13:52:23 +0900

    [C-Api] prepare new APIs
    prepare next ACR
    - function to invoke with preallocated output handle
    - function to open nn model with custom options
    
    Change function names and add description for each API.
    
    Signed-off-by: Jaeyun <jy1210.jung@samsung.com>

--- api/capi/include/nnstreamer-single.h
+++ api/capi/include/nnstreamer-single.h
@@ -76,6 +76,40 @@ typedef void *ml_single_h;
 int ml_single_open (ml_single_h *single, const char *model, const ml_tensors_info_h input_info, const ml_tensors_info_h output_info, ml_nnfw_type_e nnfw, ml_nnfw_hw_e hw);
 
 /**
+ * @brief Opens an ML model and returns the instance as a handle.
+ * @details Even if the model has flexible input data dimensions,
+ *          input data frames of an instance of a model should share the same dimension.
+ * @since_tizen 6.5
+ * @remarks %http://tizen.org/privilege/mediastorage is needed if @a model is relevant to media storage.
+ * @remarks %http://tizen.org/privilege/externalstorage is needed if @a model is relevant to external storage.
+ * @param[out] single This is the model handle opened. Users are required to close
+ *                   the given instance with ml_single_close().
+ * @param[in] model This is the path to the neural network model file.
+ * @param[in] input_info This is required if the given model has flexible input
+ *                      dimension, where the input dimension MUST be given
+ *                      before executing the model.
+ *                      It is required by some custom filters of NNStreamer.
+ *                      You may set NULL if it's not required.
+ * @param[in] output_info This is required if the given model has flexible output dimension.
+ * @param[in] nnfw The neural network framework used to open the given @a model.
+ *                 Set #ML_NNFW_TYPE_ANY to let it auto-detect.
+ * @param[in] hw Tell the corresponding @a nnfw to use a specific hardware.
+ *               Set #ML_NNFW_HW_ANY if it does not matter.
+ * @param[in] custom_option Comma separated list of options.
+ *                      It is necessary to optimize the control for some neural network framework. (e.g. NumThreads:N to set the number of threads in TensorFlow-Lite)
+ *                      You may set NULL if it's not required.
+ *                      See NNStreamer (https://github.com/nnstreamer/nnstreamer) documentation for the details.
+ * @return @c 0 on success. Otherwise a negative error value.
+ * @retval #ML_ERROR_NONE Successful.
+ * @retval #ML_ERROR_NOT_SUPPORTED Not supported.
+ * @retval #ML_ERROR_PERMISSION_DENIED The application does not have the privilege to access to the media storage or external storage.
+ * @retval #ML_ERROR_INVALID_PARAMETER Fail. The parameter is invalid.
+ * @retval #ML_ERROR_STREAMS_PIPE Failed to start the pipeline.
+ * @retval #ML_ERROR_OUT_OF_MEMORY Failed to allocate required memory.
+ */
+int ml_single_open_full (ml_single_h * single, const char *model, const ml_tensors_info_h input_info, const ml_tensors_info_h output_info, ml_nnfw_type_e nnfw, ml_nnfw_hw_e hw, const char *custom_option);
+
+/**
  * @brief Closes the opened model handle.
  * @since_tizen 5.5
  * @param[in] single The model handle to be closed.
@@ -105,6 +139,24 @@ int ml_single_close (ml_single_h single);
  * @retval #ML_ERROR_OUT_OF_MEMORY Failed to allocate required memory.
  */
 int ml_single_invoke (ml_single_h single, const ml_tensors_data_h input, ml_tensors_data_h *output);
+
+/**
+ * @brief Invokes the model with the given input data and fills the @a output data handle. This is generally faster than ml_single_invoke().
+ * @details The caller should preallocate memory buffers of the given output handle before calling the API.
+ *          Note that ml_single_invoke() allocates memory buffers of the output handle in the API, which may incur memcpy.
+ * @since_tizen 6.5
+ * @param[in] single The model handle to be inferred.
+ * @param[in] input The input data to be inferred.
+ * @param[in/out] output The output data to be filled by the API. Output should be preallocated before calling the API.
+ * @return @c 0 on success. Otherwise a negative error value.
+ * @retval #ML_ERROR_NONE Successful.
+ * @retval #ML_ERROR_NOT_SUPPORTED Not supported.
+ * @retval #ML_ERROR_INVALID_PARAMETER Fail. The parameter is invalid.
+ * @retval #ML_ERROR_STREAMS_PIPE Failed to push a buffer into source element.
+ * @retval #ML_ERROR_TIMED_OUT Failed to get the result from sink element.
+ * @retval #ML_ERROR_OUT_OF_MEMORY Failed to allocate required memory.
+ */
+int ml_single_invoke_fast (ml_single_h single, const ml_tensors_data_h input, ml_tensors_data_h output);
 
 /**
  * @brief Invokes the model with the given input data with the given tensors information.
