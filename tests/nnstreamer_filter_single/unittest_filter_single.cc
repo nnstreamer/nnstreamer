68d4c5fbafc0685d721e96ed4e15739a78f339dd
Sangjung Woo <sangjung.woo@samsung.com>
2022-04-06 10:59:46 +0900

    [Test/Single] Apply test fixture to remove duplicate code
    To remove duplicate code in each test case, this patch applies the test
    fixture framework. Moreover, g_autofree macro is used for allocated
    space to be freed automatically no matter test case is passed or not.
    
    Signed-off-by: Sangjung Woo <sangjung.woo@samsung.com>

--- tests/nnstreamer_filter_single/unittest_filter_single.cc
+++ tests/nnstreamer_filter_single/unittest_filter_single.cc
@@ -13,212 +13,192 @@
 
 #include "../gst/nnstreamer/tensor_filter/tensor_filter_single.h"
 
+#ifdef ENABLE_TENSORFLOW_LITE
 /**
- * @brief Internal data structure to run tensor-filter single.
+ * @brief Test Fixture class for a tensor-filter single functionality.
  */
-typedef struct
+class NNSFilterSingleTest : public::testing::Test
 {
+protected:
   GTensorFilterSingle *single;
   GTensorFilterSingleClass *klass;
   GstTensorMemory input;
   GstTensorMemory output;
-} single_test_data_s;
+  gboolean loaded;
 
-#ifdef ENABLE_TENSORFLOW_LITE
-/**
- * @brief Get max score from output.
- */
-static gsize
-get_max_score (GstTensorMemory * output)
-{
-  guint8 *array;
-  gsize idx, max_idx = 0;
-  guint8 max_value = 0;
+public:
+  /**
+   * @brief Construct a new NNSFilterSingleTest object
+   */
+  NNSFilterSingleTest ()
+    : single (nullptr), klass (nullptr), loaded (FALSE)
+  {
+    input.data = nullptr;
+    output.data = nullptr;
+  }
 
-  array = (guint8 *) output->data;
+  /**
+   * @brief SetUp method for each test case
+   */
+  void SetUp () override
+  {
+    g_autofree gchar *model_file = nullptr;
+    g_autofree gchar *data_file = nullptr;
+    gsize length;
+    const gchar *root_path = g_getenv ("NNSTREAMER_SOURCE_ROOT_PATH");
 
-  for (idx = 0; idx < output->size; idx++) {
-    if (max_value < array[idx]) {
-      max_idx = idx;
-      max_value = array[idx];
+    input.size = 3U * 224 * 224;
+    output.size = 1001U;
+
+    single = (GTensorFilterSingle *) g_object_new (G_TYPE_TENSOR_FILTER_SINGLE, NULL);
+    klass = (GTensorFilterSingleClass *) g_type_class_ref (G_TYPE_TENSOR_FILTER_SINGLE);
+
+    /* supposed to run test in build directory */
+    if (root_path == NULL)
+      root_path = "..";
+
+    model_file = g_build_filename (root_path, "tests", "test_models", "models",
+        "mobilenet_v1_1.0_224_quant.tflite", NULL);
+    ASSERT_TRUE (g_file_test (model_file, G_FILE_TEST_EXISTS));
+
+    data_file = g_build_filename (root_path, "tests", "test_models",
+        "data", "orange.raw", NULL);
+    ASSERT_TRUE (g_file_get_contents (data_file, (gchar **) & input.data, &length, NULL));
+    ASSERT_TRUE (length == input.size);
+
+    g_object_set (G_OBJECT (single), "framework", "tensorflow-lite",
+        "model", model_file, NULL);
+    loaded = TRUE;
+  }
+
+  /**
+   * @brief TearDown method for each test case
+   */
+  void TearDown () override
+  {
+    g_type_class_unref (klass);
+    g_object_unref (single);
+    g_free (input.data);
+    g_free (output.data);
+  }
+
+  /**
+   * @brief Get the max score in the given array.
+   * @param tensorMemory tensor memory which contains the target array.
+   * @return gsize max value in the given array
+   */
+  gsize get_max_score (GstTensorMemory *tensorMemory)
+  {
+    guint8 *array;
+    gsize idx, max_idx = 0;
+    guint8 max_value = 0;
+
+    array = (guint8 *) tensorMemory->data;
+
+    for (idx = 0; idx < tensorMemory->size; idx++) {
+      if (max_value < array[idx]) {
+        max_idx = idx;
+        max_value = array[idx];
+      }
     }
+
+    return max_idx;
   }
-
-  return max_idx;
-}
-
-/**
- * @brief Free test data.
- */
-static void
-free_test_data (single_test_data_s * test_data)
-{
-  g_type_class_unref (test_data->klass);
-  g_object_unref (test_data->single);
-  g_free (test_data->input.data);
-  g_free (test_data->output.data);
-}
-
-/**
- * @brief Initialize test data, load tf-lite model and raw data.
- */
-static gboolean
-init_test_data (single_test_data_s * test_data)
-{
-  const gchar *root_path = g_getenv ("NNSTREAMER_SOURCE_ROOT_PATH");
-  gchar *model_file, *data_file;
-  gsize length;
-  gboolean loaded = FALSE;
-
-  model_file = data_file = NULL;
-  memset (test_data, 0, sizeof (single_test_data_s));
-  test_data->input.size = 3U * 224 * 224;
-  test_data->output.size = 1001U;
-
-  test_data->single = (GTensorFilterSingle *) g_object_new (G_TYPE_TENSOR_FILTER_SINGLE, NULL);
-  test_data->klass = (GTensorFilterSingleClass *) g_type_class_ref (G_TYPE_TENSOR_FILTER_SINGLE);
-
-  /* supposed to run test in build directory */
-  if (root_path == NULL)
-    root_path = "..";
-
-  model_file = g_build_filename (root_path, "tests", "test_models", "models",
-      "mobilenet_v1_1.0_224_quant.tflite", NULL);
-  if (!g_file_test (model_file, G_FILE_TEST_EXISTS)) {
-    goto error;
-  }
-
-  data_file = g_build_filename (root_path, "tests", "test_models",
-      "data", "orange.raw", NULL);
-  if (!g_file_get_contents (data_file, (gchar **) &test_data->input.data, &length, NULL) ||
-      length != test_data->input.size) {
-    goto error;
-  }
-
-  g_object_set (G_OBJECT (test_data->single), "framework", "tensorflow-lite",
-      "model", model_file, NULL);
-  loaded = TRUE;
-
-error:
-  if (!loaded)
-    free_test_data (test_data);
-
-  g_free (model_file);
-  g_free (data_file);
-
-  return loaded;
-}
+};
 
 /**
  * @brief Test to invoke tf-lite model.
  */
-TEST (testTensorFilterSingle, invoke)
+TEST_F (NNSFilterSingleTest, invoke_p)
 {
-  single_test_data_s tdata = { 0, };
-
-  ASSERT_TRUE (init_test_data (&tdata));
+  ASSERT_TRUE (this->loaded);
 
   /* invoke the model and check label 'orange' (index 951) */
-  EXPECT_TRUE (tdata.klass->invoke (tdata.single, &tdata.input, &tdata.output, TRUE));
-  EXPECT_EQ (get_max_score (&tdata.output), 951U);
+  EXPECT_TRUE (klass->invoke (single, &input, &output, TRUE));
+  EXPECT_EQ (951U, get_max_score (&output));
 
-  EXPECT_TRUE (tdata.klass->invoke (tdata.single, &tdata.input, &tdata.output, FALSE));
-  EXPECT_EQ (get_max_score (&tdata.output), 951U);
+  EXPECT_TRUE (klass->invoke (single, &input, &output, FALSE));
+  EXPECT_EQ (951U, get_max_score (&output));
 
-  /* check status (tf-lite does not need extra info and data allocation) */
-  EXPECT_TRUE (tdata.klass->input_configured (tdata.single));
-  EXPECT_TRUE (tdata.klass->output_configured (tdata.single));
-  EXPECT_FALSE (tdata.klass->allocate_in_invoke (tdata.single));
-  tdata.klass->destroy_notify (tdata.single, &tdata.output);
+  EXPECT_TRUE (klass->input_configured (single));
+  EXPECT_TRUE (klass->output_configured (single));
+  EXPECT_FALSE (klass->allocate_in_invoke (single));
 
-  free_test_data (&tdata);
+  klass->destroy_notify (single, &output);
 }
 
 /**
  * @brief Test to invoke tf-lite model with invalid param.
  */
-TEST (testTensorFilterSingle, invokeInvalidParam_n)
+TEST_F (NNSFilterSingleTest, invokeInvalidParam_n)
 {
-  single_test_data_s tdata = { 0, };
+  ASSERT_TRUE (this->loaded);
+  EXPECT_TRUE (klass->start (single));
 
-  ASSERT_TRUE (init_test_data (&tdata));
-  EXPECT_TRUE (tdata.klass->start (tdata.single));
+  EXPECT_FALSE (klass->invoke (single, NULL, &output, FALSE));
+  EXPECT_FALSE (klass->invoke (single, &input, NULL, FALSE));
 
-  EXPECT_FALSE (tdata.klass->invoke (tdata.single, NULL, &tdata.output, FALSE));
-  EXPECT_FALSE (tdata.klass->invoke (tdata.single, &tdata.input, NULL, FALSE));
-
-  EXPECT_TRUE (tdata.klass->stop (tdata.single));
-  free_test_data (&tdata);
+  EXPECT_TRUE (klass->stop (single));
 }
 
 /**
  * @brief Test to invoke tf-lite model with invalid data.
  */
-TEST (testTensorFilterSingle, invokeInvalidSize_n)
+TEST_F (NNSFilterSingleTest, invokeInvalidSize_n)
 {
-  single_test_data_s tdata = { 0, };
-
-  ASSERT_TRUE (init_test_data (&tdata));
-  EXPECT_TRUE (tdata.klass->start (tdata.single));
+  ASSERT_TRUE (this->loaded);
+  EXPECT_TRUE (klass->start (single));
 
   /* request allocation with invalid size */
-  tdata.output.size = 0;
-  EXPECT_FALSE (tdata.klass->invoke (tdata.single, &tdata.input, &tdata.output, TRUE));
+  output.size = 0;
+  EXPECT_FALSE (klass->invoke (single, &input, &output, TRUE));
 
-  EXPECT_TRUE (tdata.klass->stop (tdata.single));
-  free_test_data (&tdata);
+  EXPECT_TRUE (klass->stop (single));
 }
 
 /**
  * @brief Test to set invalid info.
  */
-TEST (testTensorFilterSingle, setInvalidInfo_n)
+TEST_F (NNSFilterSingleTest, setInvalidInfo_n)
 {
-  single_test_data_s tdata = { 0, };
   GstTensorsInfo in_info, out_info;
-
   gst_tensors_info_init (&in_info);
   gst_tensors_info_init (&out_info);
 
-  ASSERT_TRUE (init_test_data (&tdata));
-  EXPECT_TRUE (tdata.klass->start (tdata.single));
+  ASSERT_TRUE (this->loaded);
+  EXPECT_TRUE (klass->start (single));
 
   /* valid tensor info */
   in_info.num_tensors = 1U;
   in_info.info[0].type = _NNS_UINT8;
   gst_tensor_parse_dimension ("3:224:224:1", in_info.info[0].dimension);
-  EXPECT_TRUE (tdata.klass->set_input_info (tdata.single, &in_info, &out_info) == 0);
+  EXPECT_TRUE (klass->set_input_info (single, &in_info, &out_info) == 0);
 
   /* request to set invalid tensor info */
   gst_tensor_parse_dimension ("1:1:1:1", in_info.info[0].dimension);
-  EXPECT_FALSE (tdata.klass->set_input_info (tdata.single, &in_info, &out_info) == 0);
+  EXPECT_FALSE (klass->set_input_info (single, &in_info, &out_info) == 0);
 
-  EXPECT_TRUE (tdata.klass->stop (tdata.single));
+  EXPECT_TRUE (klass->stop (single));
   gst_tensors_info_free (&in_info);
   gst_tensors_info_free (&out_info);
-  free_test_data (&tdata);
 }
 
 /**
  * @brief Test for filter property.
  */
-TEST (testTensorFilterSingle, invalidProperty_n)
+TEST_F (NNSFilterSingleTest, invalidProperty_n)
 {
-  single_test_data_s tdata = { 0, };
-  gchar *string_val = NULL;
+  g_autofree gchar *string_val = NULL;
 
-  ASSERT_TRUE (init_test_data (&tdata));
+  ASSERT_TRUE (this->loaded);
 
   /* invalid property name */
-  g_object_set (G_OBJECT (tdata.single), "framework-invalid", "invalid", NULL);
-  g_object_get (G_OBJECT (tdata.single), "framework-invalid", &string_val, NULL);
+  g_object_set (G_OBJECT (single), "framework-invalid", "invalid", NULL);
+  g_object_get (G_OBJECT (single), "framework-invalid", &string_val, NULL);
   EXPECT_FALSE (string_val != NULL);
 
-  g_object_get (G_OBJECT (tdata.single), "framework", &string_val, NULL);
+  g_object_get (G_OBJECT (single), "framework", &string_val, NULL);
   EXPECT_TRUE (string_val && g_str_equal (string_val, "tensorflow-lite"));
-  g_free (string_val);
-
-  free_test_data (&tdata);
 }
 #endif /* ENABLE_TENSORFLOW_LITE */
 
