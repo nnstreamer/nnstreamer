33cdae16399233123440e44f4ef16a40f0eccaac
Jaeyun <jy1210.jung@samsung.com>
2022-05-12 20:24:34 +0900

    [Edge] base code for edge event
    Update base code to handle edge event.
    
    Signed-off-by: Jaeyun <jy1210.jung@samsung.com>

--- gst/nnstreamer/tensor_query/nnstreamer_edge_common.c
+++ gst/nnstreamer/tensor_query/nnstreamer_edge_common.c
@@ -13,6 +13,118 @@
 #include "nnstreamer_edge_common.h"
 
 /**
+ * @brief Create nnstreamer edge event.
+ */
+int
+nns_edge_event_create (nns_edge_event_e event, nns_edge_event_h * event_h)
+{
+  nns_edge_event_s *ee;
+
+  if (!event_h) {
+    nns_edge_loge ("Invalid param, event_h should not be null.");
+    return NNS_EDGE_ERROR_INVALID_PARAMETER;
+  }
+
+  if (event <= NNS_EDGE_EVENT_UNKNOWN) {
+    nns_edge_loge ("Invalid param, given event type is invalid.");
+    return NNS_EDGE_ERROR_INVALID_PARAMETER;
+  }
+
+  ee = (nns_edge_event_s *) malloc (sizeof (nns_edge_event_s));
+  if (!ee) {
+    nns_edge_loge ("Failed to allocate memory for edge event.");
+    return NNS_EDGE_ERROR_OUT_OF_MEMORY;
+  }
+
+  memset (ee, 0, sizeof (nns_edge_event_s));
+  ee->magic = NNS_EDGE_MAGIC;
+  ee->event = event;
+
+  *event_h = ee;
+  return NNS_EDGE_ERROR_NONE;
+}
+
+/**
+ * @brief Destroy nnstreamer edge event.
+ */
+int
+nns_edge_event_destroy (nns_edge_event_h event_h)
+{
+  nns_edge_event_s *ee;
+
+  ee = (nns_edge_event_s *) event_h;
+
+  if (!NNS_EDGE_MAGIC_IS_VALID (ee)) {
+    nns_edge_loge ("Invalid param, given edge event is invalid.");
+    return NNS_EDGE_ERROR_INVALID_PARAMETER;
+  }
+
+  ee->magic = NNS_EDGE_MAGIC_DEAD;
+
+  if (ee->data.destroy_cb)
+    ee->data.destroy_cb (ee->data.data);
+
+  g_free (ee);
+  return NNS_EDGE_ERROR_NONE;
+}
+
+/**
+ * @brief Set event data.
+ */
+int
+nns_edge_event_set_data (nns_edge_event_h event_h, void *data, size_t data_len,
+    nns_edge_data_destroy_cb destroy_cb)
+{
+  nns_edge_event_s *ee;
+
+  ee = (nns_edge_event_s *) event_h;
+
+  if (!NNS_EDGE_MAGIC_IS_VALID (ee)) {
+    nns_edge_loge ("Invalid param, given edge event is invalid.");
+    return NNS_EDGE_ERROR_INVALID_PARAMETER;
+  }
+
+  if (!data || data_len <= 0) {
+    nns_edge_loge ("Invalid param, data should not be null.");
+    return NNS_EDGE_ERROR_INVALID_PARAMETER;
+  }
+
+  /* Clear old data and set new one. */
+  if (ee->data.destroy_cb)
+    ee->data.destroy_cb (ee->data.data);
+
+  ee->data.data = data;
+  ee->data.data_len = data_len;
+  ee->data.destroy_cb = destroy_cb;
+
+  return NNS_EDGE_ERROR_NONE;
+}
+
+/**
+ * @brief Get the nnstreamer edge event type.
+ */
+int
+nns_edge_event_get_type (nns_edge_event_h event_h, nns_edge_event_e * event)
+{
+  nns_edge_event_s *ee;
+
+  ee = (nns_edge_event_s *) event_h;
+
+  if (!NNS_EDGE_MAGIC_IS_VALID (ee)) {
+    nns_edge_loge ("Invalid param, given edge event is invalid.");
+    return NNS_EDGE_ERROR_INVALID_PARAMETER;
+  }
+
+  if (!event) {
+    nns_edge_loge ("Invalid param, event should not be null.");
+    return NNS_EDGE_ERROR_INVALID_PARAMETER;
+  }
+
+  *event = ee->event;
+  return NNS_EDGE_ERROR_NONE;
+}
+
+/**
  * @brief Validate data handle.
  */
 bool
