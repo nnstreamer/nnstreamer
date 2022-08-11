d10d90f43383eaebb6b8fbfffdb351b3408eab90
Yechan Choi <yechan9.choi@samsung.com>
2022-08-11 19:18:21 +0900

    [Gst/Edge] Add edgesrc and edgesink
    - Add new elements: edgesrc and edgesink.
    - Modify Test for edgesrc and edgesink.
    
    Signed-off-by: Yechan Choi <yechan9.choi@samsung.com>
    Signed-off-by: gichan <gichan2.jang@samsung.com>

--- gst/edge/edge_common.c
+++ gst/edge/edge_common.c
@@ -10,3 +10,28 @@
  * @bug     No known bugs
  *
  */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "edge_common.h"
+
+/**
+ * @brief register GEnumValue array for edge protocol property handling
+ */
+GType
+gst_edge_get_connect_type (void)
+{
+  static GType protocol = 0;
+  if (protocol == 0) {
+    static GEnumValue protocols[] = {
+      {NNS_EDGE_CONNECT_TYPE_TCP, "TCP",
+          "Directly sending stream frames via TCP connections."},
+          /** @todo support UDP, MQTT and HYBRID */
+      {0, NULL, NULL},
+    };
+    protocol = g_enum_register_static ("edge_protocol", protocols);
+  }
+
+  return protocol;
+}
