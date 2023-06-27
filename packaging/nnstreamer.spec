nnstreamer/nnstreamer

commit 2d6bc59653dab21bf1706d61a06c205c10583a92

Filter/Meson: Move 'filter-snpe-list' to the build directory
This patch moves the 'filter-snpe-list' file generated during the build
time from the source directory to the build directory.

Signed-off-by: Wook Song <wook16.song@samsung.com>

ext/nnstreamer/tensor_filter/meson.build
@@ -641,8 +641,8 @@ if get_option('enable-mediapipe')
   )
 endif
 
-run_command('sh', '-c', 'touch filter_snpe_list', check : true)
 if snpe_support_is_available
+  run_command('sh', '-c', 'touch $MESON_BUILD_ROOT/$MESON_SUBDIR/filter_snpe_list', check : true)
   filter_sub_snpe_sources = ['tensor_filter_snpe.cc']
 
   snpe_cpp_args = []
@@ -665,7 +665,7 @@ if snpe_support_is_available
     install: true,
     install_dir: nnstreamer_libdir
   )
-  run_command('sh', '-c', 'echo "%{_prefix}/lib/nnstreamer/filters/libnnstreamer_filter_snpe.so" >> filter_snpe_list', check : true)
+  run_command('sh', '-c', 'echo "%{_prefix}/lib/nnstreamer/filters/libnnstreamer_filter_snpe.so" >> $MESON_BUILD_ROOT/$MESON_SUBDIR/filter_snpe_list', check : true)
 endif
 
 if tensorrt_support_is_available

packaging/nnstreamer.spec
@@ -863,6 +863,8 @@ export NNSTREAMER_DECODERS=${NNSTREAMER_BUILD_ROOT_PATH}/ext/nnstreamer/tensor_d
 export NNSTREAMER_CONVERTERS=${NNSTREAMER_BUILD_ROOT_PATH}/ext/nnstreamer/tensor_converter
 export NNSTREAMER_TRAINERS=${NNSTREAMER_BUILD_ROOT_PATH}/ext/nnstreamer/tensor_trainer
 
+%define files_opetion_for_snpe_subpackage %( if [ -f %{builddir}/ext/nnstreamer/tensor_filter/filter_snpe_list ] ; then \
+	echo "-f %{builddir}/ext/nnstreamer/tensor_filter/filter_snpe_list" ; else echo ""; fi )
 %define test_script $(pwd)/packaging/run_unittests_binaries.sh
 
 # if it's tizen && non-TV, run unittest even if "unit_test"==0 for build-time sanity checks.
@@ -1095,7 +1097,7 @@ cp -r result %{buildroot}%{_datadir}/nnstreamer/unittest/
 %if 0%{?snpe_support}
 # Workaround: Conditionally enable nnstreamer-snpe rpm package
 # when existing actual snpe library (snpe.pc)
-%files snpe -f ext/nnstreamer/tensor_filter/filter_snpe_list
+%files snpe %{files_opetion_for_snpe_subpackage}
 %manifest nnstreamer.manifest
 %defattr(-,root,root,-)
 %endif

commit da7b89e17714f1058d02e91061a150984547d56e

Dist/Tizen: Fix a GBS build error due to #4007
This patch fixes a GBS build error caused by #4007 on the SNPE
sub-plugin for Tizen.

Signed-off-by: Wook Song <wook16.song@samsung.com>

ext/nnstreamer/tensor_filter/meson.build
@@ -641,7 +641,6 @@ if get_option('enable-mediapipe')
   )
 endif
 
-run_command('sh', '-c', 'touch filter_snpe_list', check : true)
 if snpe_support_is_available
   filter_sub_snpe_sources = ['tensor_filter_snpe.cc']
 
@@ -665,7 +664,6 @@ if snpe_support_is_available
     install: true,
     install_dir: nnstreamer_libdir
   )
-  run_command('sh', '-c', 'echo "%{_prefix}/lib/nnstreamer/filters/libnnstreamer_filter_snpe.so" >> filter_snpe_list', check : true)
 endif
 
 if tensorrt_support_is_available

packaging/nnstreamer.spec
@@ -944,6 +944,13 @@ ln -sf %{_libdir}/nnstreamer_python3.so nnstreamer_python.so
 popd
 %endif
 
+%if 0%{?snpe_support}
+%define snpe_sub_plugin libnnstreamer_filter_snpe.so
+%define files_for_snpe_subpackage files_for_snpe_subpackage.txt
+rm -f %{files_for_snpe_subpackage}
+find %{buildroot} -type f -name '%{snpe_sub_plugin}' -printf '/%%P\n' > %{files_for_snpe_subpackage}
+%endif
+
 %if 0%{?testcoverage}
 ##
 # The included directories are:
@@ -1105,7 +1112,7 @@ cp -r result %{buildroot}%{_datadir}/nnstreamer/unittest/
 %if 0%{?snpe_support}
 # Workaround: Conditionally enable nnstreamer-snpe rpm package
 # when existing actual snpe library (snpe.pc)
-%files snpe -f ext/nnstreamer/tensor_filter/filter_snpe_list
+%files snpe -f %{files_for_snpe_subpackage}
 %manifest nnstreamer.manifest
 %defattr(-,root,root,-)
 %endif