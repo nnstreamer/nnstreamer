c12c9c83072cc2e7847c6e392ea0b947cb45eab6
Yelin Jeong <yelini.jeong@samsung.com>
2023-09-21 12:09:34 +0900

    [Doc] edit documentation files related with tensor info
    This patch edits docs and comments related with tensor info.
    
    Signed-off-by: Yelin Jeong <yelini.jeong@samsung.com>

--- gst/nnstreamer/include/tensor_typedef.h
+++ gst/nnstreamer/include/tensor_typedef.h
@@ -235,7 +235,7 @@ typedef struct
                    User must designate this in a few NNFW frameworks (tensorflow)
                    and some (tensorflow-lite) do not need this. */
   tensor_type type; /**< Type of each element in the tensor. User must designate this. */
-  tensor_dim dimension; /**< Dimension. We support up to 4th ranks.  */
+  tensor_dim dimension; /**< Dimension. We support up to 16th ranks.  */
 } GstTensorInfo;
 
 /**
