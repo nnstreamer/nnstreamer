9e9ca80f0baa398b1aded6f0bb2074214d075b55
Jaeyun Jung <jy1210.jung@samsung.com>
2024-01-02 14:29:09 +0900

    [CodeClean/grpc] clearly define release function
    The type of data pushed into queue is gst-buffer.
    Clearly set the release function when calling free.
    
    Signed-off-by: Jaeyun Jung <jy1210.jung@samsung.com>

--- ext/nnstreamer/extra/nnstreamer_grpc_common.cc
+++ ext/nnstreamer/extra/nnstreamer_grpc_common.cc
@@ -193,7 +193,7 @@ void
 NNStreamerRPC::_data_queue_item_free (GstDataQueueItem *item)
 {
   if (item->object)
-    gst_mini_object_unref (item->object);
+    gst_buffer_unref (GST_BUFFER (item->object));
   g_free (item);
 }
 
