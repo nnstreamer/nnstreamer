aca32e63d92da3ad65b95d334c32f46dd39a9f52
Yongjoo Ahn <yongjoo1.ahn@samsung.com>
2024-08-09 16:03:55 +0900

    [build] Exit with proper value in tensorrt-version.sh
    - Exit tensorrt-version.sh with non zero value to meson detect it.
    
    Signed-off-by: Yongjoo Ahn <yongjoo1.ahn@samsung.com>

--- tools/tensorrt-version.sh
+++ tools/tensorrt-version.sh
@@ -2,7 +2,7 @@
 set -e
 NV_INFER_VERSION_H=$(find /usr/include -iname "NvInferVersion.h")
 if [ -z ${NV_INFER_VERSION_H} ]; then
-  exit
+  exit 1
 fi
 NV_TENSORRT_MAJOR=$(cat ${NV_INFER_VERSION_H} | grep NV_TENSORRT_MAJOR | awk '{ print $3 }')
 NV_TENSORRT_MINOR=$(cat ${NV_INFER_VERSION_H} | grep NV_TENSORRT_MINOR | awk '{ print $3 }')
