7937e3adab80bbda0cc93d9f598318a879758d14
gichan2-jang <gichan2.jang@samsung.com>
2024-08-28 20:28:14 +0900

    [TDD] Add edgesrc/sink and query custom connection tests
    Add edgesrc/sink and tensor_qeury custom connection tests.
    Enable the tests later.
    
    Signed-off-by: gichan2-jang <gichan2.jang@samsung.com>

--- tests/nnstreamer_edge/query/unittest_query.cc
+++ tests/nnstreamer_edge/query/unittest_query.cc
@@ -15,6 +15,8 @@
 #include <unittest_util.h>
 #include "../gst/nnstreamer/tensor_query/tensor_query_common.h"
 
+static const char *CUSTOM_LIB_PATH = "./libnnstreamer-edge-custom-test.so";
+
 /**
  * @brief Test for tensor_query_server get and set properties
  */
@@ -151,66 +153,6 @@ TEST (tensorQuery, serverProperties2_n)
 }
 
 /**
- * @brief Test for tensor_query_client get and set properties
- */
-TEST (tensorQuery, clientProperties0)
-{
-  gchar *pipeline;
-  GstElement *gstpipe;
-  GstElement *client_handle;
-  nns_edge_connect_type_e connect_type;
-  guint uint_val;
-  gchar *str_val;
-  gboolean bool_val;
-
-  /* Create a query client pipeline */
-  pipeline = g_strdup_printf (
-      "videotestsrc ! videoconvert ! videoscale ! video/x-raw,width=300,height=300,format=RGB !"
-      "tensor_converter ! tensor_query_client name=client connect-type=TCP ! tensor_sink");
-  gstpipe = gst_parse_launch (pipeline, NULL);
-  EXPECT_NE (gstpipe, nullptr);
-
-  /* Get properties of query client */
-  client_handle = gst_bin_get_by_name (GST_BIN (gstpipe), "client");
-  EXPECT_NE (client_handle, nullptr);
-
-  g_object_get (client_handle, "host", &str_val, NULL);
-  EXPECT_STREQ ("localhost", str_val);
-  g_free (str_val);
-
-  g_object_get (client_handle, "port", &uint_val, NULL);
-  EXPECT_EQ (3001U, uint_val);
-
-  g_object_get (client_handle, "connect-type", &connect_type, NULL);
-  EXPECT_EQ (connect_type, NNS_EDGE_CONNECT_TYPE_TCP);
-
-  g_object_get (client_handle, "silent", &bool_val, NULL);
-  EXPECT_EQ (TRUE, bool_val);
-
-  /* Set properties of query client */
-  g_object_set (client_handle, "host", "127.0.0.2", NULL);
-  g_object_get (client_handle, "host", &str_val, NULL);
-  EXPECT_STREQ ("127.0.0.2", str_val);
-  g_free (str_val);
-
-  g_object_set (client_handle, "port", 5001U, NULL);
-  g_object_get (client_handle, "port", &uint_val, NULL);
-  EXPECT_EQ (5001U, uint_val);
-
-  g_object_set (client_handle, "timeout", 1000U, NULL);
-  g_object_get (client_handle, "timeout", &uint_val, NULL);
-  EXPECT_EQ (1000U, uint_val);
-
-  g_object_set (client_handle, "silent", FALSE, NULL);
-  g_object_get (client_handle, "silent", &bool_val, NULL);
-  EXPECT_EQ (FALSE, bool_val);
-
-  gst_object_unref (client_handle);
-  gst_object_unref (gstpipe);
-  g_free (pipeline);
-}
-
-/**
  * @brief Test for tensor_query_server run.
  */
 TEST (tensorQuery, serverRun)
@@ -261,6 +203,244 @@ TEST (tensorQuery, clientAlone_n)
   EXPECT_NE (gstpipe, nullptr);
 
   EXPECT_NE (setPipelineStateSync (gstpipe, GST_STATE_PLAYING, UNITTEST_STATECHANGE_TIMEOUT), 0);
+
+  gst_object_unref (gstpipe);
+  g_free (pipeline);
+}
+
+
+/**
+ * @brief Test for tensor_query_server custom connection
+ */
+TEST (tensorQuery, serverCustomNormal)
+{
+  /** @todo TDD: Enable this test later. */
+  GTEST_SKIP ();
+
+  gchar *pipeline = nullptr;
+  GstElement *gstpipe = nullptr;
+  guint src_port;
+
+  src_port = get_available_port ();
+
+  /* Create a nnstreamer pipeline */
+  pipeline = g_strdup_printf (
+      "tensor_query_serversrc connect-type=CUSTOM custom-lib=%s name=serversrc port=%u ! "
+      "other/tensors,num_tensors=1,dimensions=3:300:300:1,types=uint8 ! "
+      "tensor_query_serversink connect-type=CUSTOM custom-lib=%s name=serversink",
+      CUSTOM_LIB_PATH, src_port, CUSTOM_LIB_PATH);
+  gstpipe = gst_parse_launch (pipeline, nullptr);
+  EXPECT_NE (gstpipe, nullptr);
+
+
+  EXPECT_EQ (setPipelineStateSync (gstpipe, GST_STATE_PLAYING, UNITTEST_STATECHANGE_TIMEOUT), 0);
+  g_usleep (1000000);
+
+  EXPECT_EQ (setPipelineStateSync (gstpipe, GST_STATE_NULL, UNITTEST_STATECHANGE_TIMEOUT), 0);
+  g_usleep (100000);
+
+  gst_object_unref (gstpipe);
+  g_free (pipeline);
+}
+
+/**
+ * @brief Test for tensor_query_server custom connection with invalid property.
+ */
+TEST (tensorQuery, serverCustomInvalidProp_n)
+{
+  /** @todo TDD: Enable this test later. */
+  GTEST_SKIP ();
+
+  gchar *pipeline = nullptr;
+  GstElement *gstpipe = nullptr;
+  guint src_port;
+
+  src_port = get_available_port ();
+
+  /* Create a nnstreamer pipeline */
+  pipeline = g_strdup_printf (
+      "tensor_query_serversrc connect-type=CUSTOM name=serversrc port=%u ! "
+      "other/tensors,num_tensors=1,dimensions=3:300:300:1,types=uint8 ! "
+      "tensor_query_serversink connect-type=CUSTOM custom-lib=%s name=serversink",
+      src_port, CUSTOM_LIB_PATH);
+  gstpipe = gst_parse_launch (pipeline, nullptr);
+  EXPECT_NE (gstpipe, nullptr);
+
+
+  EXPECT_NE (setPipelineStateSync (gstpipe, GST_STATE_PLAYING, UNITTEST_STATECHANGE_TIMEOUT), 0);
+  g_usleep (1000000);
+
+  gst_object_unref (gstpipe);
+  g_free (pipeline);
+}
+
+/**
+ * @brief Test for tensor_query_server custom connection with invalid property.
+ */
+TEST (tensorQuery, serverCustomInvalidProp2_n)
+{
+  /** @todo TDD: Enable this test later. */
+  GTEST_SKIP ();
+
+  gchar *pipeline = nullptr;
+  GstElement *gstpipe = nullptr;
+  guint src_port = 0;
+
+  src_port = get_available_port ();
+
+  /* Create a nnstreamer pipeline */
+  pipeline = g_strdup_printf (
+      "tensor_query_serversrc connect-type=CUSTOM custom-lib=%s name=serversrc port=%u ! "
+      "other/tensors,num_tensors=1,dimensions=3:300:300:1,types=uint8 ! "
+      "tensor_query_serversink connect-type=CUSTOM  name=serversink",
+      CUSTOM_LIB_PATH, src_port);
+  gstpipe = gst_parse_launch (pipeline, nullptr);
+  EXPECT_NE (gstpipe, nullptr);
+
+
+  EXPECT_NE (setPipelineStateSync (gstpipe, GST_STATE_PLAYING, UNITTEST_STATECHANGE_TIMEOUT), 0);
+  g_usleep (1000000);
+
+  gst_object_unref (gstpipe);
+  g_free (pipeline);
+}
+
+/**
+ * @brief Test for tensor_query_server custom connection with invalid property.
+ */
+TEST (tensorQuery, serverCustomInvalidProp3_n)
+{
+  /** @todo TDD: Enable this test later. */
+  GTEST_SKIP ();
+
+  gchar *pipeline = nullptr;
+  GstElement *gstpipe = nullptr;
+  guint src_port = 0;
+
+  src_port = get_available_port ();
+
+  /* Create a nnstreamer pipeline */
+  pipeline = g_strdup_printf (
+      "tensor_query_serversrc connect-type=CUSTOM custom-lib=INVALID.so name=serversrc port=%u ! "
+      "other/tensors,num_tensors=1,dimensions=3:300:300:1,types=uint8 ! "
+      "tensor_query_serversink connect-type=CUSTOM custom-lib=%s name=serversink",
+      src_port, CUSTOM_LIB_PATH);
+  gstpipe = gst_parse_launch (pipeline, nullptr);
+  EXPECT_NE (gstpipe, nullptr);
+
+
+  EXPECT_NE (setPipelineStateSync (gstpipe, GST_STATE_PLAYING, UNITTEST_STATECHANGE_TIMEOUT), 0);
+  g_usleep (1000000);
+
+  gst_object_unref (gstpipe);
+  g_free (pipeline);
+}
+
+/**
+ * @brief Test for tensor_query_server custom connection with invalid property.
+ */
+TEST (tensorQuery, serverCustomInvalidProp4_n)
+{
+  /** @todo TDD: Enable this test later. */
+  GTEST_SKIP ();
+
+  gchar *pipeline = nullptr;
+  GstElement *gstpipe = nullptr;
+  guint src_port = 0;
+
+  src_port = get_available_port ();
+
+  /* Create a nnstreamer pipeline */
+  pipeline = g_strdup_printf (
+      "tensor_query_serversrc connect-type=CUSTOM custom-lib=%s name=serversrc port=%u ! "
+      "other/tensors,num_tensors=1,dimensions=3:300:300:1,types=uint8 ! "
+      "tensor_query_serversink connect-type=CUSTOM custom-lib=INVALID.so name=serversink",
+      CUSTOM_LIB_PATH, src_port);
+  gstpipe = gst_parse_launch (pipeline, nullptr);
+  EXPECT_NE (gstpipe, nullptr);
+
+  EXPECT_NE (setPipelineStateSync (gstpipe, GST_STATE_PLAYING, UNITTEST_STATECHANGE_TIMEOUT), 0);
+  g_usleep (1000000);
+
+  gst_object_unref (gstpipe);
+  g_free (pipeline);
+}
+
+/**
+ * @brief Test for tensor_query_client custom connection
+ */
+TEST (tensorQuery, customClientNormal)
+{
+  /** @todo TDD: Enable this test later. */
+  GTEST_SKIP ();
+
+  gchar *pipeline = nullptr;
+  GstElement *gstpipe = nullptr;
+
+  /* Create a query client pipeline */
+  pipeline = g_strdup_printf ("videotestsrc ! videoconvert ! videoscale ! video/x-raw,width=300,height=300,format=RGB !"
+                              "tensor_converter ! tensor_query_client connect-type=CUSTOM custom-lib=%s "
+                              "name=client connect-type=TCP ! tensor_sink",
+      CUSTOM_LIB_PATH);
+  gstpipe = gst_parse_launch (pipeline, nullptr);
+  EXPECT_NE (gstpipe, nullptr);
+
+  EXPECT_EQ (setPipelineStateSync (gstpipe, GST_STATE_PLAYING, UNITTEST_STATECHANGE_TIMEOUT), 0);
+  g_usleep (1000000);
+
+  EXPECT_EQ (setPipelineStateSync (gstpipe, GST_STATE_NULL, UNITTEST_STATECHANGE_TIMEOUT), 0);
+  g_usleep (100000);
+
+  gst_object_unref (gstpipe);
+  g_free (pipeline);
+}
+
+/**
+ * @brief Test for tensor_query_client custom connection with invalid property.
+ */
+TEST (tensorQuery, customClientInvalidProp_n)
+{
+  /** @todo TDD: Enable this test later. */
+  GTEST_SKIP ();
+
+  gchar *pipeline = nullptr;
+  GstElement *gstpipe = nullptr;
+
+  /* Create a query client pipeline */
+  pipeline = g_strdup_printf ("videotestsrc ! videoconvert ! videoscale ! video/x-raw,width=300,height=300,format=RGB !"
+                              "tensor_converter ! tensor_query_client connect-type=CUSTOM "
+                              "name=client connect-type=TCP ! tensor_sink");
+  gstpipe = gst_parse_launch (pipeline, nullptr);
+  EXPECT_NE (gstpipe, nullptr);
+
+  EXPECT_NE (setPipelineStateSync (gstpipe, GST_STATE_PLAYING, UNITTEST_STATECHANGE_TIMEOUT), 0);
+  g_usleep (1000000);
+
+  gst_object_unref (gstpipe);
+  g_free (pipeline);
+}
+
+/**
+ * @brief Test for tensor_query_client custom connection with invalid property.
+ */
+TEST (tensorQuery, customClientInvalidProp2_n)
+{
+  /** @todo TDD: Enable this test later. */
+  GTEST_SKIP ();
+
+  gchar *pipeline = nullptr;
+  GstElement *gstpipe = nullptr;
+
+  /* Create a query client pipeline */
+  pipeline = g_strdup_printf (
+      "videotestsrc ! videoconvert ! videoscale ! video/x-raw,width=300,height=300,format=RGB !"
+      "tensor_converter ! tensor_query_client connect-type=CUSTOM custom-lib=INVALID.so "
+      "name=client connect-type=TCP ! tensor_sink");
+  gstpipe = gst_parse_launch (pipeline, nullptr);
+  EXPECT_NE (gstpipe, nullptr);
+
+  EXPECT_NE (setPipelineStateSync (gstpipe, GST_STATE_PLAYING, UNITTEST_STATECHANGE_TIMEOUT), 0);
+  g_usleep (1000000);
 
   gst_object_unref (gstpipe);
   g_free (pipeline);
