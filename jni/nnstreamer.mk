1f7e2999eb276812f6e1244acd61f74d021d8309
Yelin Jeong <yelini.jeong@samsung.com>
2024-12-19 09:57:54 +0900

    [android] Build llama.cpp tensor_filter
    This patch exposes the source code to build api/android.
    
    Signed-off-by: Yelin Jeong <yelini.jeong@samsung.com>

--- jni/nnstreamer.mk
+++ jni/nnstreamer.mk
@@ -134,6 +134,10 @@ NNSTREAMER_FILTER_MXNET_SRCS := \
 NNSTREAMER_FILTER_LLAMA2C_SRCS := \
     $(NNSTREAMER_EXT_HOME)/tensor_filter/tensor_filter_llama2.cc
 
+# filter llamacpp
+NNSTREAMER_FILTER_LLAMACPP_SRCS := \
+    $(NNSTREAMER_EXT_HOME)/tensor_filter/tensor_filter_llamacpp.cc
+
 # converter flatbuffers
 NNSTREAMER_CONVERTER_FLATBUF_SRCS := \
     $(NNSTREAMER_EXT_HOME)/tensor_converter/tensor_converter_util.c \
