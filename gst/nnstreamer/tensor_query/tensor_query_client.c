dcfca8ee83aba2f5ee07bb9c2af957849b6bce8b
Gichan Jang <gichan2.jang@samsung.com>
2025-03-24 14:13:11 +0900

    Resolve svace issue
     - Fix resource leak
     - Fix deferencing null pointer.
    
    Signed-off-by: Gichan Jang <gichan2.jang@samsung.com>

--- gst/nnstreamer/tensor_query/tensor_query_client.c
+++ gst/nnstreamer/tensor_query/tensor_query_client.c
@@ -521,11 +521,12 @@ static gboolean
 gst_tensor_query_client_create_edge_handle (GstTensorQueryClient * self)
 {
   gboolean started = FALSE;
-  gchar *prev_caps = NULL;
   int ret;
 
   /* Already created, compare caps string. */
   if (self->edge_h) {
+    gchar *prev_caps = NULL;
+
     ret = nns_edge_get_info (self->edge_h, "CAPS", &prev_caps);
 
     if (ret != NNS_EDGE_ERROR_NONE || !prev_caps ||
@@ -533,7 +534,9 @@ gst_tensor_query_client_create_edge_handle (GstTensorQueryClient * self)
       /* Capability is changed, close old handle. */
       nns_edge_release_handle (self->edge_h);
       self->edge_h = NULL;
+      g_free (prev_caps);
     } else {
+      g_free (prev_caps);
       return TRUE;
     }
   }
--- tests/nnstreamer_edge/nnstreamer-edge-custom-test.c
+++ tests/nnstreamer_edge/nnstreamer-edge-custom-test.c
@@ -102,22 +102,39 @@ nns_edge_custom_connect (void *priv)
   nns_edge_custom_test_s *custom_h;
   nns_edge_data_h data_h;
   gchar *raw_data;
+  int ret = NNS_EDGE_ERROR_INVALID_PARAMETER;
+
   if (!priv) {
     nns_loge ("Invalid param, handle should not be null.");
-    return NNS_EDGE_ERROR_INVALID_PARAMETER;
+    return ret;
   }
   custom_h = (nns_edge_custom_test_s *) priv;
   custom_h->is_connected = 1;
 
   /* Push dummy buffer to launch GstBaseSrc */
-  nns_edge_data_create (&data_h);
+  ret = nns_edge_data_create (&data_h);
+  if (ret != NNS_EDGE_ERROR_NONE) {
+    nns_loge ("Failed to create edge data handle.");
+    return ret;
+  }
+
   raw_data = g_strdup ("Dummy data");
-  nns_edge_data_add (data_h, raw_data, strlen (raw_data) + 1, g_free);
-  nns_edge_event_invoke_callback (custom_h->event_cb, custom_h->user_data,
+  ret = nns_edge_data_add (data_h, raw_data, strlen (raw_data) + 1, g_free);
+  if (ret != NNS_EDGE_ERROR_NONE) {
+    nns_loge ("Failed to add edge data.");
+    g_free (raw_data);
+    goto done;
+  }
+  ret = nns_edge_event_invoke_callback (custom_h->event_cb, custom_h->user_data,
       NNS_EDGE_EVENT_NEW_DATA_RECEIVED, data_h, sizeof (nns_edge_data_h), NULL);
+  if (ret != NNS_EDGE_ERROR_NONE) {
+    nns_loge ("Failed to invoke edge event.");
+  }
+
+done:
   nns_edge_data_destroy (data_h);
 
-  return NNS_EDGE_ERROR_NONE;
+  return ret;
 }
 
 static int
