7da8fd72e1f1fe28c88c9388b58ddb19e305b516
Yongjoo Ahn <yongjoo1.ahn@samsung.com>
2025-06-11 19:16:45 +0900

    [filter][vivante] Support model-json based initialization
    - Let `gst_tensor_filter_detect_framework` allows vivante to take 1 value for model_files
    
    Signed-off-by: Yongjoo Ahn <yongjoo1.ahn@samsung.com>

--- gst/nnstreamer/tensor_filter/tensor_filter_common.c
+++ gst/nnstreamer/tensor_filter/tensor_filter_common.c
@@ -1215,6 +1215,8 @@ gst_tensor_filter_detect_framework (const gchar * const *model_files,
       detected_fw = g_strdup ("openvino");
     else if (g_str_equal (ext[0], ".tvn"))
       detected_fw = g_strdup ("trix-engine");
+    else if (g_str_equal (ext[0], ".nb"))
+      detected_fw = g_strdup ("vivante");
   } else if (num_models == 2) {
     if (g_str_equal (ext[0], ".pb") && g_str_equal (ext[1], ".pb") &&
         !g_str_equal (model_files[0], model_files[1]))
